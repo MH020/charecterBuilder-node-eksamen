d724c2b08da9ba5a0f9f84286e31511ca197850c
Martin <166133291+MH020@users.noreply.github.com>
2025-12-05 09:45:09 +0100

    db works if run from root backend

--- backend/db/connection.js
+++ backend/db/connection.js
@@ -1,20 +1,31 @@
+import 'dotenv/config'
 import pg from 'pg'
 const { Pool, Client } = pg
-const connectionString = 'postgresql://dbuser:secretpassword@database.server.com:3211/mydb'
+
+const prod = process.argv.includes('prod')
+let connectionString;
+
+
+const ssl = { rejectUnauthorized: false }
+
+
+if (prod){
+  connectionString = process.env.DB_URI_PROD
+} else {
+  connectionString = process.env.DB_URI_DEV
+}
+
+console.log(connectionString)
  
 const pool = new Pool({
-  connectionString,
+  connectionString, ssl
 })
  
-await pool.query('SELECT NOW()')
-await pool.end()
- 
-const client = new Client({
-  connectionString,
-})
- 
-await client.connect()
- 
-await client.query('SELECT NOW()')
- 
-await client.end()
+try {
+  const res = await pool.query('SELECT NOW()')
+  console.log(res.rows)
+} catch (err) {
+  console.error(err)
+} finally {
+  await pool.end()
+}
