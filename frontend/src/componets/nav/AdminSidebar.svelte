3101f1358675826f9e7376e856d53ff41fca1b90
Martin <166133291+MH020@users.noreply.github.com>
2025-12-08 13:20:24 +0100

    Update AdminSidebar.svelte

--- frontend/src/componets/nav/AdminSidebar.svelte
+++ frontend/src/componets/nav/AdminSidebar.svelte
@@ -1,10 +1,10 @@
 <script>
     import { fetchGet, fetchModal } from "../../../util/fetchUtil";
     import toastrDisplayHTTPCode from "../../../util/ToastrUtil";
-	import CharectersheetsList from "../lists/charectersheetsList.svelte";
 	import BaseModal from "../modal/BaseModal.svelte";
 	import {modalStore, openModal, closeModal} from "../../store/modalStore.js"
-
+	import CharectersheetsList from "../lists/charectersheetsList.svelte";
+	import SkillsList from "../lists/skillsList.svelte";
 
 
 	async function loadcharectersheets() {
@@ -12,6 +12,10 @@
 	}
 
     function displayPlayers() {
+        fetchModal("/users", "players");
+    }
+
+    function displayskills() {
         fetchModal("/users", "players");
     }
 
@@ -31,6 +35,10 @@
 			<img alt="players" class="sidebar-button" />
 		</button>
 
+		<button class="icon-button" on:click={displayskills}>
+			<img alt="players" class="sidebar-button" />
+		</button>
+
   </nav>
 </aside>
 
@@ -39,6 +47,8 @@
 	{#if $modalStore.page === "charectersheet"}
 		<CharectersheetsList data={$modalStore.data} />
 	{:else if $modalStore.page === "players"}
-
-	{/if}
+	<CharectersheetsList data={$modalStore.data} />
+	{:else if $modalStore.page === "skills"}
+	<SkillsList data={$modalStore.data} />
+	{/if} 
 </BaseModal>
