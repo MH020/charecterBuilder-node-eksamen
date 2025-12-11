ab4e6a655d4f75c26473af8d143d2d088645d2c9
Martin <166133291+MH020@users.noreply.github.com>
2025-12-11 13:50:25 +0100

    weapons router

--- backend/routers/weapons/weaponsRouter.js
+++ backend/routers/weapons/weaponsRouter.js
@@ -9,10 +9,9 @@ const router = Router()
 
 router.get("/api/weapons", async (req, res) => {
     try {
-        const query = `
-            SELECT 
-                weapon.id, weapon.type, weapon.name, weapon.range, weapon.hands, weapon.rof, weapon.damage, weapon.pen, 
-                weapon.clip, weapon.reload, weapon.wt, weapon.is_custom,
+        const result = await db.query(`
+            SELECT weapon.id, weapon.type, weapon.name, weapon.range, weapon.hands, weapon.rof, weapon.damage, weapon.pen, 
+                   weapon.clip, weapon.reload, weapon.wt, weapon.is_custom,
 
                 json_build_object(
                     'id', weapon_class.id,
@@ -33,9 +32,8 @@ router.get("/api/weapons", async (req, res) => {
             JOIN weapon_class ON weapon.weapon_class_id = weapon_class.id
             JOIN category ON weapon.category_id = category.id
             JOIN availability ON weapon.availability_id = availability.id;
-        `;
+        `);
 
-        const result = await db.query(query);
         const weapons = result.rows;
 
         return res.status(200).json(weapons);
@@ -48,17 +46,18 @@ router.get("/api/weapons", async (req, res) => {
 
 router.post("/api/weapons", async (req,res) => {
     try {
-        const { name } = req.body
+        const { weapon_class, type, category, name, hands, rof, damage, pen, clip, reload, wt, availability, projectile} = req.body;
 
-        if (!name ) {
+        if (!name ) { //add more 
         return res.status(400).send({ message: 'missing fields' })
         }
 
         const is_custom = req.session.user?.role === "ADMIN" || false
 
             const result = await db.query(
-            `INSERT INTO aptitude ("name") VALUES ($1) RETURNING *`,
-            [name]
+            `INSERT INTO weapon (weapon_class_id, type, category_id, "name", hands, rof, damage, pen, clip, reload, wt, availability_id , projectile_id) 
+            VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11 ,$12 ,$13, $14) RETURNING *`,
+            [weapon_class.id, type, category.id, name, hands, rof, damage, pen, clip, reload, wt, availability.id, projectile.id , is_custom]
         )
         const createdAptitude = result.rows[0];
         return res.status(201).send({ message: 'weapon created sucessfully', created: createdAptitude})
@@ -72,9 +71,14 @@ router.post("/api/weapons", async (req,res) => {
 router.put("/api/weapons/:id", async (req, res) => {
     try {
         const id = req.params.id;
-        const { name } = req.body;
+        const { weapon_class_id,type, category_id, name, hands, rof, damage, pen, clip, reload, wt, availability_id, projectile_id } = req.body;
 
-        await db.query(`UPDATE aptitude SET name = $1 WHERE id = $2`, [name, id]);
+
+        const is_custom = req.session.user?.role === "ADMIN" || false
+
+        await db.query( `UPDATE weapon SET weapon_class_id = $1, type = $2, category_id = $3, name = $4, hands = $5, rof = $6, damage = $7, pen = $8,
+        clip = $9, reload = $10, wt = $11, availability_id = $12, projectile_id = $13, is_custom =$14, WHERE id = $15 RETURNING *`, 
+        [weapon_class.id, type, category.id, name, hands, rof, damage, pen, clip, reload, wt, availability.id, projectile.id , is_custom, id]);
 
         return res.status(200).send({ message: 'aptitude updated' })
         
@@ -90,7 +94,7 @@ router.delete("/api/weapons/:id", async (req, res) => {
         console.log("skillid?", id)
 
         await db.query(
-            `DELETE FROM aptitude where id = $1 `, [id],
+            `DELETE FROM weapon where id = $1 `, [id],
         )
 
         return res.status(200).send({ message: 'aptitude deleted' })
