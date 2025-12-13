65bbbb1940db63aecca2c557dc7e1217ea911a98
Martin <166133291+MH020@users.noreply.github.com>
2025-12-13 14:17:39 +0100

    Update weaponTraitList.svelte

---
+++ frontend/src/componets/lists/weaponLists/weaponTraitList.svelte
@@ -0,0 +1,66 @@
+<script>
+    import { onMount } from 'svelte';
+    import { fetchDelete, fetchGet } from '../../../../util/fetchUtil';
+    import toastrDisplayHTTPCode from '../../../../util/ToastrUtil';
+    import WeaponClassRow from '../../rows/weaponClassRow.svelte';
+
+    let weaponTraits = [];
+    let sortType = "all"; 
+    let sortedTraits= [];
+
+ 
+    onMount(async () => {
+        const response = await fetchGet("/api/weapon/traits")
+        if (response.status === 200) {
+                weaponTraits = response.data
+        } 
+    });
+
+
+
+    function createWeaponTraits(){
+        const newWeaponTrait = {
+            id: null,  
+            name: "null",        
+            description: "", 
+            isNew: true
+
+        };
+        weaponTraits = [...weaponTraits, newWeaponTrait];
+    }
+
+    async function deleteWeaponTrait(weaponTrait){
+        const response = await fetchDelete("/api/category", weaponTrait.id)
+        toastrDisplayHTTPCode(response.status, response.message)
+        if (response.status === 200){
+            weaponTraits = weaponTraits.filter(wc => wc.id !== weaponTrait.id)
+        }
+    }
+
+    //sorting needed 
+
+    function toggleSort() {
+
+    }
+</script>
+
+<div class="button-panel">
+    <button on:click={toggleSort}>
+        sort by... {sortType === "all" ? "A -> Z" : "Z -> A"}
+    </button>
+    <button on:click={createWeaponTraits}>
+        new Weapon trait
+    </button>
+</div>
+
+<div>
+    {#each weaponTraits as weaponClass (weaponClass.id)}
+        <WeaponClassRow weaponClass={weaponClass} 
+        deleteWeaponClass={() => deleteWeaponTrait(weaponClass)}
+        isEditing={weaponClass.isNew}/>
+    {/each}
+</div>
+
+
+
+
