28efe1fcc6356751ced9c2a900bcdca488b30f88
Martin <166133291+MH020@users.noreply.github.com>
2025-12-16 15:32:17 +0100

    Update 02_availability.js

--- backend/knex/seeds/02_availability.js
+++ backend/knex/seeds/02_availability.js
@@ -4,13 +4,6 @@
  */
 export async function seed (knex) {
   // Deletes ALL existing entries
-  await knex('weapon_weapon_traits').del()
-  await knex('weapon_weapon_class').del()
-  await knex('weapon_upgrade').del()
-  await knex('weapon').del()
-  await knex('item').del()
-    await knex('armor').del()
-  await knex('availability').del()
 
   await knex('availability').insert([
     { name: 'Common' },
