2c7be03dd7c92c1b0887c056d8efc82533471eeb
Martin <166133291+MH020@users.noreply.github.com>
2025-12-16 18:45:40 +0100

    Create lineageBonusCard.svelte

---
+++ frontend/src/componets/cards/lineageBonusCard.svelte
@@ -0,0 +1,48 @@
+<script>
+    import { onMount } from "svelte";
+    import { closeModal,modalSelectCallback } from "../../store/modalStore";
+    import { fetchGet } from "../../../util/fetchUtil";
+
+    export let lineageBonus;
+    let options = [];
+    let selectedOption = null;
+
+    onMount(async () => {
+        const response = await fetchGet("/api/lineage");
+        if (response.status === 200) {
+            options = response.data;
+        }
+    });
+
+
+    $: if (selectedOption) {
+        lineageBonus.characteristic_id = selectedOption.id;
+        lineageBonus.characteristic_name = selectedOption.name;
+    }
+</script>
+
+<div class="row">
+    <select bind:value={selectedOption}>
+    <option value="" disabled selected>Select an characteristic</option>
+    {#each options as option}
+        <option value={option.id}>{option.name}</option>
+    {/each}
+    </select>
+    <p>Selected ID: {selectedOption ? selectedOption.name : 'None'}</p>
+
+    <div class="cell-box">
+        <div class="label">description</div>
+        <input bind:value={lineageBonus.bonus} />
+    </div>
+
+    <div class="buttons">
+        <button
+            on:click={() => {
+                $modalSelectCallback(lineageBonus);
+                closeModal();
+            }}
+        >
+            select</button
+        >
+    </div>
+</div>
