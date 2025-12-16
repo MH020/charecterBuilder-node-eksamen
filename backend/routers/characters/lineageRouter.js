f55252c01702d7567e646992f0452e32999ebb0b
Martin <166133291+MH020@users.noreply.github.com>
2025-12-16 15:44:42 +0100

    update lineage router post

--- backend/routers/characters/lineageRouter.js
+++ backend/routers/characters/lineageRouter.js
@@ -23,24 +23,24 @@ router.get('/api/lineage', async (req, res) => {
 
         const baseLineages = result.rows
 
-        async function getLineageBonuses(id){
-        const result = await db.query(`
+        async function getLineageBonuses(id) {
+            const result = await db.query(`
             SELECT ccb.characteristic_id, c.name AS characteristic_name, ccb.bonus, ccb.is_custom
             FROM lineage_characteristic_bonus ccb
             JOIN characteristic c ON ccb.characteristic_id = c.id
             WHERE ccb.lineage_id = $1
         `, [id]);
-        return result.rows;
+            return result.rows;
         };
 
         async function getLineageAptitudes(id) {
-        const result = await db.query(`
+            const result = await db.query(`
             SELECT la.aptitude_id, a.name AS aptitude_name
             FROM lineage_aptitude la
             JOIN aptitude a ON la.aptitude_id = a.id
             WHERE la.lineage_id = $1
         `, [id]);
-        return result.rows;
+            return result.rows;
         };
 
 
@@ -59,7 +59,7 @@ router.get('/api/lineage', async (req, res) => {
 
 router.post('/api/lineage', async (req, res) => {
     try {
-        const { name, description, defining_features, race } = req.body
+        const { name, description, defining_features, race, aptitudes, bonuses } = req.body
 
         if (!name) {
             return res.status(400).send({ message: 'missing fields' })
@@ -72,8 +72,18 @@ router.post('/api/lineage', async (req, res) => {
             VALUES ($1, $2, $3, $4, $5) RETURNING *`,
             [name, description, defining_features, race.id, is_custom]
         )
-        const createdArmor = result.rows[0];
-        return res.status(201).send({ message: 'lineage created sucessfully', created: createdArmor })
+        const createdLineage = result.rows[0];
+
+        for (const bonus of bonuses) {
+            await db.query('INSERT INTO lineage_characteristic_bonus (lineage_id, characteristic_id, bonus, is_custom) VALUES ($1, $2, $3, $4)',
+                [createdLineage.id, bonus.characteristic_id, bonus.bonus, is_custom])
+        }
+
+        for (const apptitude of aptitudes) {
+            await db.query('INSERT INTO lineage_aptitude (lineage_id, aptitude_id) VALUES ($1, $2)',
+                [ createdLineage.id, apptitude.id ])
+        }
+        return res.status(201).send({ message: 'lineage created sucessfully', created: createdLineage })
     } catch (error) {
         console.error(error)
         return res.status(500).send({ message: 'server error', error: error.message })
