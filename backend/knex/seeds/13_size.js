f73192f6e17a0e596a96e16984260bcea8f8ef33
Martin <166133291+MH020@users.noreply.github.com>
2025-12-16 15:32:25 +0100

    Create 13_size.js

---
+++ backend/knex/seeds/13_size.js
@@ -0,0 +1,36 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+export async function seed(knex) {
+  // Deletes ALL existing entries
+  await knex('size').del();
+
+  // Inserts seed entries (IDs auto-generated)
+  await knex('size').insert([
+    {
+      name: 'Tiny',
+      score: 2
+    },
+    {
+      name: 'Small',
+      score: 3
+    },
+    {
+      name: 'Medium',
+      score: 4
+    },
+    {
+      name: 'Large',
+      score: 6
+    },
+    {
+      name: 'Huge',
+      score: 8
+    },
+    {
+      name: 'Gargantuan',
+      score: 10
+    }
+  ]);
+}
