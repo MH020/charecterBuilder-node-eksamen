ba737492a244d59a5c4fc3afeac67f19eea24c9e
Martin <166133291+MH020@users.noreply.github.com>
2025-12-16 15:32:09 +0100

    Update 00_characteristic.js

--- backend/knex/seeds/00_characteristic.js
+++ backend/knex/seeds/00_characteristic.js
@@ -1,13 +1,55 @@
 /**
  * @param { import("knex").Knex } knex
- * @returns { Promise<void> } 
+ * @returns { Promise<void> }
  */
 export async function seed(knex) {
   // Deletes ALL existing entries
-  await knex('table_name').del()
-  await knex('table_name').insert([
-    {id: 1, colName: 'rowValue1'},
-    {id: 2, colName: 'rowValue2'},
-    {id: 3, colName: 'rowValue3'}
+  await knex('weapon_weapon_traits').del()
+  await knex('weapon_weapon_class').del()
+  await knex('lineage_characteristic_bonus').del()
+    await knex('lineage_aptitude').del()
+  await knex('weapon_upgrade').del()
+  await knex('weapon').del()
+  await knex('item').del()
+  await knex('armor').del()
+  await knex('availability').del()
+  await knex('characteristic').del();
+  await knex('lineage').del();
+  await knex('race').del();
+
+  // Inserts seed entries (IDs auto-generated)
+  await knex('characteristic').insert([
+    {
+      name: 'Weapon Skill',
+      description: 'Represents a model’s skill in melee combat.'
+    },
+    {
+      name: 'Ballistic Skill',
+      description: 'Represents a model’s accuracy with ranged weapons.'
+    },
+    {
+      name: 'Strength',
+      description: 'Measures how physically powerful a model is.'
+    },
+    {
+      name: 'Toughness',
+      description: 'Represents a model’s ability to resist damage.'
+    },
+    {
+      name: 'Wounds',
+      description: 'The amount of damage a model can sustain before being destroyed.'
+    },
+    {
+      name: 'Attacks',
+      description: 'The number of melee attacks a model can make.'
+    },
+    {
+      name: 'Leadership',
+      description: 'Represents a model’s morale and discipline.'
+    },
+    {
+      name: 'Save',
+      description: 'The armor save a model uses to resist incoming damage.'
+    }
   ]);
-};
+}
