f622842fb64111c8bd84abee8e9b1912deefa6d0
Martin <166133291+MH020@users.noreply.github.com>
2025-12-17 13:36:23 +0100

    Update RaceList.svelte

---
+++ frontend/src/componets/lists/characterLists/RaceList.svelte
@@ -0,0 +1,103 @@
+<script>
+    import { onMount } from "svelte";
+    import { fetchGet } from "../../../../util/fetchUtil";
+    import { deleteEntity } from "../../../../util/ListUtil";
+    import StatlineRow from "../../rows/StatlineRow.svelte";
+    import RaceRow from "../../rows/RaceRow.svelte";
+
+
+    let races = [];
+    let sortType = "all";
+    let sortedRaces = [];
+    const endpoint = "/api/races"
+
+    onMount(async () => {
+        const response = await fetchGet(endpoint);
+        console.log(response);
+        if (response.status === 200) {
+            races = response.data;
+        }
+    });
+
+    function createRace() {
+        const newRace = {
+            id: null,
+            name: "",
+            description: "",
+            wounds: 0, 
+            base_statline: {},
+            max_statline: {},
+            size: {},
+            isNew: true
+        };
+        races = [...races, newRace];
+    }
+
+    async function deleteRace(id, isNew = false) {
+        if (isNew) {
+            races = races.filter(
+                (race) => race.id !== id,
+            );
+            return;
+        }
+        races = await deleteEntity(id, endpoint, races);
+    }
+
+    $: if (races) {
+        sortedRaces = [...races].sort((a, b) => {
+            if (sortType === "all") {
+                if (a.name < b.name) {
+                    return 1;
+                }
+                if (a.name > b.name) {
+                    return -1;
+                }
+                return 0;
+            } else {
+                if (a.name < b.name) {
+                    return -1;
+                }
+                if (a.name > b.name) {
+                    return 1;
+                }
+                return 0;
+            }
+        });
+    }
+
+    function updateStatlines(updated) {
+        races = races.map((statline) => {
+            if (statline.isNew) {
+                return { ...updated, isNew: false };
+            }
+
+            if (statline.id === updated.id) {
+                return { ...statline, ...updated };
+            }
+
+            return statline;
+        });
+    }
+
+    function toggleSort() {
+        sortType = sortType === "all" ? "asc" : "all";
+    }
+</script>
+
+<div class="button-panel">
+    <button on:click={toggleSort}>
+        Sort {sortType === "all" ? "A -> Z" : "Z -> A"}
+    </button>
+    <button on:click={createRace}> new statline </button>
+</div>
+
+<div>
+    {#each sortedRaces as race (race.id)}
+        <RaceRow
+            race={race}
+            onSave={updateStatlines}
+            onDelete={deleteRace}
+            endpoint={endpoint}
+        />
+    {/each}
+</div>
