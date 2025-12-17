9404d74176b59737a234703f4e960f199edc3c54
Martin <166133291+MH020@users.noreply.github.com>
2025-12-17 16:09:15 +0100

    Update statlineRaceRouter.js

--- backend/routers/characters/statlineRaceRouter.js
+++ backend/routers/characters/statlineRaceRouter.js
@@ -5,85 +5,86 @@ import db from '../../db/connection.js'
 const router = Router()
 
 router.get('/api/races/statlines', async (req, res) => {
-    try {
-        const result = await db.query(`
+  try {
+    const result = await db.query(`
             SELECT id, name, weapon_skill, ballistic_skill, strength, toughness, agility, intelligence, perception, willpower, fellowship, is_custom
             FROM statline_race;
-        `);
-        const statlines = result.rows
+        `)
+    const statlines = result.rows
 
-        return res.status(200).send(statlines)
-    } catch (error) {
-        console.error(error)
-        return res.status(500).send({ message: 'server error', error: error.message })
-    }
+    return res.status(200).send(statlines)
+  } catch (error) {
+    console.error(error)
+    return res.status(500).send({ message: 'server error', error: error.message })
+  }
 })
 
 router.post('/api/races/statlines', async (req, res) => {
-    try {
-        const { name, weapon_skill, ballistic_skill, strength, toughness, agility,
-            intelligence, perception, willpower, fellowship
-        } = req.body;
+  try {
+    const {
+      name, weapon_skill, ballistic_skill, strength, toughness, agility,
+      intelligence, perception, willpower, fellowship
+    } = req.body
 
-        if (!name) {
-            return res.status(400).send({ message: 'missing fields' })
-        }
+    if (!name) {
+      return res.status(400).send({ message: 'missing fields' })
+    }
 
-        const is_custom = req.session.user?.role === 'ADMIN'
+    const is_custom = req.session.user?.role === 'ADMIN'
 
-        const result = await db.query(
+    const result = await db.query(
             `INSERT INTO statline_race (
         name, weapon_skill, ballistic_skill, strength,toughness, agility, intelligence, perception,
         willpower, fellowship, is_custom)
         VALUES ($1,$2,$3,$4,$5,$6,$7,$8,$9,$10,$11) RETURNING *`,
 
             [name, weapon_skill, ballistic_skill, strength, toughness, agility,
-                intelligence, perception, willpower, fellowship, is_custom
+              intelligence, perception, willpower, fellowship, is_custom
             ]
-        );
-        const createdRace = result.rows[0];
+    )
+    const createdRace = result.rows[0]
 
-
-        return res.status(201).send({ message: 'statline created sucessfully', created: createdRace })
-    } catch (error) {
-        console.error(error)
-        return res.status(500).send({ message: 'server error', error: error.message })
-    }
+    return res.status(201).send({ message: 'statline created sucessfully', created: createdRace })
+  } catch (error) {
+    console.error(error)
+    return res.status(500).send({ message: 'server error', error: error.message })
+  }
 })
 
 router.put('/api/races/statlines/:id', async (req, res) => {
-    try {
-        const { id } = req.params
-        const { name, weapon_skill, ballistic_skill, strength, toughness, agility,
-            intelligence, perception, willpower, fellowship
-        } = req.body;
+  try {
+    const { id } = req.params
+    const {
+      name, weapon_skill, ballistic_skill, strength, toughness, agility,
+      intelligence, perception, willpower, fellowship
+    } = req.body
 
-        await db.query(`UPDATE statline_race SET
+    await db.query(`UPDATE statline_race SET
                         name = $1, weapon_skill = $2, ballistic_skill = $3, strength = $4, toughness = $5,
                         agility = $6, intelligence = $7, perception = $8, willpower = $9, fellowship = $10 
                         WHERE id = $11`,
-                [name, weapon_skill, ballistic_skill, strength, toughness, agility,
-                    intelligence, perception, willpower, fellowship, id  
-                ])
+    [name, weapon_skill, ballistic_skill, strength, toughness, agility,
+      intelligence, perception, willpower, fellowship, id
+    ])
 
-        return res.status(200).send({ message: 'statline updated' })
-    } catch (error) {
-        console.error(error)
-        return res.status(500).send({ message: 'server error', error: error.message })
-    }
+    return res.status(200).send({ message: 'statline updated' })
+  } catch (error) {
+    console.error(error)
+    return res.status(500).send({ message: 'server error', error: error.message })
+  }
 })
-//needs to check if race has statline before delete   
+// needs to check if race has statline before delete
 router.delete('/api/races/statlines/:id', async (req, res) => {
-    try {
-        const { id } = req.params
+  try {
+    const { id } = req.params
 
-        await db.query('DELETE FROM statline_race where id = $1 ', [id])
+    await db.query('DELETE FROM statline_race where id = $1 ', [id])
 
-        return res.status(200).send({ message: 'statline race deleted' })
-    } catch (error) {
-        console.error(error)
-        return res.status(500).send({ message: 'server error', error: error.message })
-    }
+    return res.status(200).send({ message: 'statline race deleted' })
+  } catch (error) {
+    console.error(error)
+    return res.status(500).send({ message: 'server error', error: error.message })
+  }
 })
 
 export default router
