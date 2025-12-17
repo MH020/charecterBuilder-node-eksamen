89c6826d8b77bfb9045a9dd0d1fdeee778964d75
Martin <166133291+MH020@users.noreply.github.com>
2025-12-17 16:08:55 +0100

    Update lineage_aptitude_join.js

--- backend/knex/seeds/lineage_aptitude_join.js
+++ backend/knex/seeds/lineage_aptitude_join.js
@@ -2,23 +2,23 @@
  * @param { import("knex").Knex } knex
  * @returns { Promise<void> }
  */
-export async function seed(knex) {
+export async function seed (knex) {
   // Deletes ALL existing entries
-  await knex('lineage_aptitude').del();
+  await knex('lineage_aptitude').del()
 
   // ---- Resolve Aptitudes ----
-  const agility = await knex('aptitude').where({ name: 'Agility' }).first();
-  const awareness = await knex('aptitude').where({ name: 'Awareness' }).first();
-  const strength = await knex('aptitude').where({ name: 'Strength' }).first();
-  const intelligence = await knex('aptitude').where({ name: 'intelligence' }).first();
-  const charisma = await knex('aptitude').where({ name: 'Charisma' }).first();
+  const agility = await knex('aptitude').where({ name: 'Agility' }).first()
+  const awareness = await knex('aptitude').where({ name: 'Awareness' }).first()
+  const strength = await knex('aptitude').where({ name: 'Strength' }).first()
+  const intelligence = await knex('aptitude').where({ name: 'intelligence' }).first()
+  const charisma = await knex('aptitude').where({ name: 'Charisma' }).first()
 
   // ---- Resolve Lineages ----
-  const hiveWorlder = await knex('lineage').where({ name: 'Hive Worlder' }).first();
-  const forgeWorlder = await knex('lineage').where({ name: 'Forge Worlder' }).first();
-  const imperialWorlder = await knex('lineage').where({ name: 'Imperial Worlder' }).first();
-  const ultramarines = await knex('lineage').where({ name: 'Ultramarines' }).first();
-  const spaceWolves = await knex('lineage').where({ name: 'Space Wolves' }).first();
+  const hiveWorlder = await knex('lineage').where({ name: 'Hive Worlder' }).first()
+  const forgeWorlder = await knex('lineage').where({ name: 'Forge Worlder' }).first()
+  const imperialWorlder = await knex('lineage').where({ name: 'Imperial Worlder' }).first()
+  const ultramarines = await knex('lineage').where({ name: 'Ultramarines' }).first()
+  const spaceWolves = await knex('lineage').where({ name: 'Space Wolves' }).first()
 
   // ---- Insert Lineage Aptitudes ----
   await knex('lineage_aptitude').insert([
@@ -68,5 +68,5 @@ export async function seed(knex) {
       lineage_id: spaceWolves.id,
       aptitude_id: awareness.id
     }
-  ]);
+  ])
 };
