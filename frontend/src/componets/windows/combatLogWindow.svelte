aeb8e0b9f5f1294856a503f806328325adf464d6
Martin <166133291+MH020@users.noreply.github.com>
2025-12-18 13:19:58 +0100

    Create combatLogWindow.svelte

---
+++ frontend/src/componets/windows/combatLogWindow.svelte
@@ -0,0 +1,16 @@
+<script>
+    import io from 'socket.io-client';
+    import { onMount } from 'svelte';
+    
+
+    const BASE_URL = import.meta.env.VITE_BASE_URL || window.location.origin
+
+    let socket;
+
+        onMount(() => {
+        socket = io(BASE_URL, {
+            withCredentials: true
+        });
+    });
+
+</script>
