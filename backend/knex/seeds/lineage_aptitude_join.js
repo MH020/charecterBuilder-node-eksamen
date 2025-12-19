e8a8867f679075d6bbb1411c36482cd9573e7365
Martin <166133291+MH020@users.noreply.github.com>
2025-12-19 08:44:02 +0100

    Update lineage_aptitude_join.js

--- backend/knex/seeds/lineage_aptitude_join.js
+++ backend/knex/seeds/lineage_aptitude_join.js
@@ -6,23 +6,22 @@ export async function seed (knex) {
   // Deletes ALL existing entries
   await knex('lineage_aptitude').del()
 
-  // ---- Resolve Aptitudes ----
+
   const agility = await knex('aptitude').where({ name: 'Agility' }).first()
   const awareness = await knex('aptitude').where({ name: 'Awareness' }).first()
   const strength = await knex('aptitude').where({ name: 'Strength' }).first()
   const intelligence = await knex('aptitude').where({ name: 'intelligence' }).first()
   const charisma = await knex('aptitude').where({ name: 'Charisma' }).first()
 
-  // ---- Resolve Lineages ----
+
   const hiveWorlder = await knex('lineage').where({ name: 'Hive Worlder' }).first()
   const forgeWorlder = await knex('lineage').where({ name: 'Forge Worlder' }).first()
   const imperialWorlder = await knex('lineage').where({ name: 'Imperial Worlder' }).first()
   const ultramarines = await knex('lineage').where({ name: 'Ultramarines' }).first()
   const spaceWolves = await knex('lineage').where({ name: 'Space Wolves' }).first()
 
-  // ---- Insert Lineage Aptitudes ----
   await knex('lineage_aptitude').insert([
-    // ===== HUMAN LINEAGES =====
+
     {
       lineage_id: hiveWorlder.id,
       aptitude_id: agility.id
@@ -50,7 +49,7 @@ export async function seed (knex) {
       aptitude_id: awareness.id
     },
 
-    // ===== ASTARTES LINEAGES =====
+
     {
       lineage_id: ultramarines.id,
       aptitude_id: intelligence.id
