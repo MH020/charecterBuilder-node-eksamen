5516008b02d0303b31b469905b0e32e78dda6343
Martin <166133291+MH020@users.noreply.github.com>
2025-12-23 23:01:38 +0100

    Update apptitudesRow.svelte

--- frontend/src/componets/rows/apptitudesRow.svelte
+++ frontend/src/componets/rows/apptitudesRow.svelte
@@ -1,87 +1,58 @@
 <script>
-  import { fetchPost, fetchUpdate } from "../../../util/fetchUtil";
   import { closeModal, modalSelectCallback } from "../../store/modalStore";
+  import EditableRowSlot from "../util/EditableRowSlot.svelte";
 
   export let apptitude;
   export let onSave;
   export let onDelete;
-  export let endpoint; 
-
-  let isEditing = apptitude.isNew;
+  export let endpoint;
   let editableApptitude = structuredClone(apptitude);
-
-  function startEditing() {
-    isEditing = true;
-  }
-
-  async function saveEdit() {
-    let updated;
-
-    if (apptitude.isNew) {
-      const response = await fetchPost("/api/aptitudes", editableApptitude);
-      if (response.status === 201) {
-        updated = response.created;
-      }
-    } else {
-      await fetchUpdate("/api/aptitudes", editableApptitude);
-      updated = editableApptitude;
-    }
-
-    onSave(updated);
-    isEditing = false;
-  }
-
-  function cancelEdit() {
-    isEditing = false;
-
-    if (apptitude.isNew) {
-      onDelete(apptitude.id, true);
-    } else {
-      editableApptitude = structuredClone(apptitude);
-    }
-  }
-
-  function deleteRow() {
-    onDelete(apptitude.id);
-  }
 </script>
 
-<div class="row">
-  {#if isEditing}
-    <div class="cell-box">
-      <div class="label">Name</div>
-      <input bind:value={editableApptitude.name} />
-    </div>
+<EditableRowSlot
+  item={apptitude}
+  endpoint={endpoint}
+  onSave={onSave}
+  onDelete={onDelete}
+  bind:editable = {editableApptitude}
+  let:isEditing
+  let:startEditing
+  let:save
+  let:cancel
+  let:remove
+>
+  <div class="row">
+    {#if isEditing}
+      <div class="cell-box">
+        <div class="label">Name</div>
+        <input bind:value={editableApptitude.name} />
+      </div>
 
-    <div class="buttons">
-      <button on:click={saveEdit}>Save</button>
+      <div class="buttons">
+        <button on:click={save}>Save</button>
+        <button on:click={cancel}>Cancel</button>
+      </div>
+    {:else}
+      <div class="cell-box">
+        <div class="label">Name</div>
+        <div>{apptitude.name || "----"}</div>
+      </div>
 
-      <button on:click={cancelEdit}>Cancel</button>
-    </div>
-  {:else}
-    <div class="cell-box">
-      <div class="label">Name</div>
-      <div>{apptitude.name || "----"}</div>
-    </div>
-
-
-    <div class="buttons">
-      {#if !$modalSelectCallback}
-        <button on:click={startEditing}>Edit</button>
-        <button on:click={deleteRow}>Delete </button>
-      {:else}
-        <button
-          on:click={() => {
-            $modalSelectCallback(apptitude);
-            closeModal();
-          }}
-        >
-          select</button
-        >
-      {/if}
-    </div>
-  {/if}
-</div>
-
-
-
+      <div class="buttons">
+        {#if !$modalSelectCallback}
+          <button on:click={startEditing}>Edit</button>
+          <button on:click={remove}>Delete</button>
+        {:else}
+          <button
+            on:click={() => {
+              $modalSelectCallback(apptitude);
+              closeModal();
+            }}
+          >
+            Select
+          </button>
+        {/if}
+      </div>
+    {/if}
+  </div>
+</EditableRowSlot>
