3065ca13825b89160457f4141ce901fa7d37adca
Martin <166133291+MH020@users.noreply.github.com>
2025-12-23 21:33:05 +0100

    Update availabilityList.svelte

--- frontend/src/componets/lists/ItemsList/availabilityList.svelte
+++ frontend/src/componets/lists/ItemsList/availabilityList.svelte
@@ -3,20 +3,19 @@
     import { fetchDelete, fetchGet } from '../../../../util/fetchUtil';
     import toastrDisplayHTTPCode from '../../../../util/ToastrUtil';
     import AvailabilityRow from '../../rows/availabilityRow.svelte';
+    import ListUtil from '../../util/ListUtil.svelte';
 
     let availabilityies = [];
     let sortType = "asc";
+    let endpoint = "/api/availability"
 
     onMount(async () => {
-        const response = await fetchGet("/api/availability");
+        const response = await fetchGet(endpoint);
         if (response.status === 200) {
             availabilityies = response.data;
         }
     });
 
-    function toggleSort() {
-        sortType = sortType === "asc" ? "desc" : "asc";
-    }
 
     $: sortedAvailabilityies = [...availabilityies].sort((a, b) => {
         if (!a.name || !b.name) return 0;
@@ -35,48 +34,11 @@
         availabilityies = [...availabilityies, newAvailability];
     }
 
-    async function deleteAvailability(id) {
-        const response = await fetchDelete("/api/availability", id);
-        toastrDisplayHTTPCode(response.status, response.message);
-
-        if (response.status === 200) {
-            availabilityies = availabilityies.filter(availability => availability.id !== id);
-        }
-    }
-
-        function updateAvailability(updated) {
-        availabilityies = availabilityies.map((availability) => {
-
-            if (availability.isNew) {
-                return {...updated, isNew: false};
-            }
-
-            if (availability.id === updated.id) {
-                return {...availability,...updated};
-            }
-
-
-            return availability;
-        });
-    }
-
-    
 </script>
 
-<div class="button-panel">
-    <button on:click={toggleSort}>
-        sort by... {sortType === "asc" ? "A → Z" : "Z → A"}
-    </button>
-
-    <button on:click={createAvailability}>
-        new availability
-    </button>
-</div>
-
-{#each sortedAvailabilityies as availability (availability.id)}
-    <AvailabilityRow
-        {availability}
-        onSave={updateAvailability}
-        onDelete={deleteAvailability}
-    />
-{/each}
+<ListUtil
+    bind:list={availabilityies}
+    endpoint={endpoint}
+    createRow={createAvailability}
+    RowComponent={AvailabilityRow}
+/>
