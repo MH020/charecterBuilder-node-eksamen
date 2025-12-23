da90faa4d5cec8d0531e792a6fd1afc3f6db3f2a
Martin <166133291+MH020@users.noreply.github.com>
2025-12-23 21:33:11 +0100

    Update itemsList.svelte

--- frontend/src/componets/lists/ItemsList/itemsList.svelte
+++ frontend/src/componets/lists/ItemsList/itemsList.svelte
@@ -3,27 +3,18 @@
     import { fetchGet } from '../../../../util/fetchUtil';
     import { deleteEntity } from '../../../../util/ListUtil';
     import ItemsRow from '../../rows/ItemsRow.svelte';
+    import ListUtil from '../../util/ListUtil.svelte';
 
     let items = [];
     let sortType = "asc";
+    let endpoint = "/api/items"
 
     onMount(async () => {
-        const response = await fetchGet("/api/items");
+        const response = await fetchGet(endpoint);
         if (response.status === 200) {
             items = response.data;
             console.log(items)
         }
-    });
-
-    function toggleSort() {
-        sortType = sortType === "asc" ? "desc" : "asc";
-    }
-
-    $: sortedItems = [...items].sort((a, b) => {
-        if (!a.name || !b.name) return 0;
-        return sortType === "asc"
-            ? a.name.localeCompare(b.name)
-            : b.name.localeCompare(a.name);
     });
 
     function createItem() {
@@ -39,48 +30,13 @@
         items = [...items, newItem];
     }
 
-    async function deleteItem(id, isNew = false) {
-        if(isNew){
-
-            items = items.filter(weaponTrait => weaponTrait.id !== id);
-            return;
-        }
-        
-        items = await deleteEntity(id,"/api/items",items);
-    }
-
-    function updateItems(updated) {
-        items = items.map((item) => {
-            if (item.isNew) {
-                return { ...updated, isNew: false };
-            }
-
-            if (item.id === updated.id) {
-                return { ...item, ...updated };
-            }
-
-            return item;
-        });
-    }
-
-    //sorting needed 
-
 
 </script>
 
-<div class="button-panel">
-    <button on:click={toggleSort}>
-        sort by... {sortType === "all" ? "A -> Z" : "Z -> A"}
-    </button>
-    <button on:click={createItem}>
-        new Weapon trait
-    </button>
-</div>
+<ListUtil
+    bind:list={items}
+    endpoint={endpoint}
+    createRow={createItem}
+    RowComponent={ItemsRow}
+/>
 
-
-{#each items as item (item.id)}
-        <ItemsRow {item}
-        onSave={updateItems}
-        onDelete={deleteItem}
-    />
-{/each}
