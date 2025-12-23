0e703bf4c3ea2a36e044bd2385e78a56bd7c10d5
Martin <166133291+MH020@users.noreply.github.com>
2025-12-23 21:33:03 +0100

    Update TalentList.svelte

--- frontend/src/componets/lists/characterLists/TalentList.svelte
+++ frontend/src/componets/lists/characterLists/TalentList.svelte
@@ -1,20 +1,16 @@
 <script>
     import { onMount } from "svelte";
     import { fetchGet } from "../../../../util/fetchUtil";
-    import { deleteEntity } from "../../../../util/ListUtil";
-    import StatlineRow from "../../rows/StatlineRow.svelte";
-    import RaceRow from "../../rows/RaceRow.svelte";
     import TalentRow from "../../rows/TalentRow.svelte";
+    import ListUtil from "../../util/ListUtil.svelte";
 
-
-    let talents = []; 
-    let sortType = "all";
-    let sortedTalents = [];
-    const endpoint = "/api/talents"
+    let talents = [];
+    const endpoint = "/api/talents";
 
     onMount(async () => {
         const response = await fetchGet(endpoint);
-        if (response.status === 200 ) {
+        console.log(response);
+        if (response.status === 200) {
             talents = response.data;
         }
     });
@@ -24,82 +20,21 @@
             id: null,
             name: "",
             description: "",
-            type: "", 
+            type: "",
             asi: false,
             prerequisite_talent: {},
             required_race: {},
             required_lineage: {},
             aptitudes: [],
-            isNew: true
+            isNew: true,
         };
         talents = [...talents, newTalent];
     }
-
-    async function deleteTalent(id, isNew = false) {
-        if (isNew) {
-            talents = talents.filter(
-                (talent) => talent.id !== id,
-            );
-            return;
-        }
-        talents = await deleteEntity(id, endpoint, talents);
-    }
-
-    $: if (talents) {
-        sortedTalents = [...talents].sort((a, b) => {
-            if (sortType === "all") {
-                if (a.name < b.name) {
-                    return 1;
-                }
-                if (a.name > b.name) {
-                    return -1;
-                }
-                return 0;
-            } else {
-                if (a.name < b.name) {
-                    return -1;
-                }
-                if (a.name > b.name) {
-                    return 1;
-                }
-                return 0;
-            }
-        });
-    }
-
-    function updateTalents(updated) {
-        talents = talents.map((talent) => {
-            if (talent.isNew) {
-                return { ...updated, isNew: false };
-            }
-
-            if (talent.id === updated.id) {
-                return { ...talent, ...updated };
-            }
-
-            return talent;
-        });
-    }
-
-    function toggleSort() {
-        sortType = sortType === "all" ? "asc" : "all";
-    }
 </script>
 
-<div class="button-panel">
-    <button on:click={toggleSort}>
-        Sort {sortType === "all" ? "A -> Z" : "Z -> A"}
-    </button>
-    <button on:click={createTalent}> new talent </button>
-</div>
-
-<div>
-    {#each sortedTalents as talent (talent.name)}
-        <TalentRow
-            talent={talent}
-            onSave={updateTalents}
-            onDelete={deleteTalent}
-            endpoint={endpoint}
-        />
-    {/each}
-</div>
+<ListUtil
+    bind:list={talents}
+    endpoint={endpoint}
+    createRow={createTalent}
+    RowComponent={TalentRow}
+/>
