9cdf61862c0b198c12960895fa863531540387d6
Martin <166133291+MH020@users.noreply.github.com>
2025-12-23 21:33:08 +0100

    Update craftmanshipList.svelte

--- frontend/src/componets/lists/ItemsList/craftmanshipList.svelte
+++ frontend/src/componets/lists/ItemsList/craftmanshipList.svelte
@@ -3,28 +3,18 @@
     import { fetchGet } from "../../../../util/fetchUtil";
     import { deleteEntity } from "../../../../util/ListUtil";
     import CraftsmanshipRow from "../../rows/craftsmanshipRow.svelte";
+    import ListUtil from "../../util/ListUtil.svelte";
     
 
     let craftmanships = [];
     let sortType = "asc";
+    let endpoint = "/api/craftsmanship"
 
     onMount(async () => {
-        const response = await fetchGet("/api/craftsmanship");
+        const response = await fetchGet(endpoint);
         if (response.status === 200) {
             craftmanships = response.data;
-            console.log(craftmanships);
         }
-    });
-
-    function toggleSort() {
-        sortType = sortType === "asc" ? "desc" : "asc";
-    }
-
-    $: sortedItems = [...craftmanships].sort((a, b) => {
-        if (!a.name || !b.name) return 0;
-        return sortType === "asc"
-            ? a.name.localeCompare(b.name)
-            : b.name.localeCompare(a.name);
     });
 
     function createCraftmanship() {
@@ -42,48 +32,11 @@
         craftmanships = [...craftmanships, newCraftmanship];
     }
 
-    async function deleteCraftmanship(id, isNew = false) {
-        if (isNew) {
-            craftmanships = craftmanships.filter(
-                (weaponTrait) => weaponTrait.id !== id,
-            );
-            return;
-        }
-
-        craftmanships = await deleteEntity(
-            id,
-            "/api/craftsmanship",
-            craftmanships,
-        );
-    }
-
-    function updateCraftmanship(updated) {
-        craftmanships = craftmanships.map((item) => {
-            if (item.isNew) {
-                return { ...updated, isNew: false };
-            }
-
-            if (item.id === updated.id) {
-                return { ...item, ...updated };
-            }
-
-            return item;
-        });
-    }
-
-    //sorting needed
 </script>
-<div class="button-panel">
-    <button on:click={toggleSort}>
-        sort by... {sortType === "all" ? "A -> Z" : "Z -> A"}
-    </button>
-    <button on:click={createCraftmanship}> new Weapon trait </button>
-</div>
 
-{#each craftmanships as craftmanship (craftmanship.id)}
-    <CraftsmanshipRow
-        {craftmanship}
-        onSave={updateCraftmanship}
-        onDelete={deleteCraftmanship}
-    />
-{/each}
+<ListUtil
+    bind:list={craftmanships}
+    endpoint={endpoint}
+    createRow={createCraftmanship}
+    RowComponent={CraftsmanshipRow}
+/>
