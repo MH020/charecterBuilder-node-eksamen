1fe55133bfa453c42431b255efce24c903336e00
Martin <166133291+MH020@users.noreply.github.com>
2025-12-23 21:33:00 +0100

    Update StatlinesList.svelte

--- frontend/src/componets/lists/characterLists/StatlinesList.svelte
+++ frontend/src/componets/lists/characterLists/StatlinesList.svelte
@@ -1,16 +1,16 @@
 <script>
     import { onMount } from "svelte";
     import { fetchGet } from "../../../../util/fetchUtil";
-    import { deleteEntity } from "../../../../util/ListUtil";
     import StatlineRow from "../../rows/StatlineRow.svelte";
+    import ListUtil from "../../util/ListUtil.svelte";
 
 
     let Statlines = [];
-    let sortType = "all";
-    let sortedStatlines = [];
+ 
+    const endpoint = "/api/statlines"
 
     onMount(async () => {
-        const response = await fetchGet("/api/races/statlines");
+        const response = await fetchGet(endpoint);
         console.log(response);
         if (response.status === 200) {
             Statlines = response.data;
@@ -34,75 +34,12 @@
         };
         Statlines = [...Statlines, newStatline];
     }
-
-    async function deleteStatline(id, isNew = false) {
-        if (isNew) {
-            Statlines = Statlines.filter(
-                (apptitude) => apptitude.id !== id,
-            );
-            return;
-        }
-        Statlines = await deleteEntity(
-            id,
-            "/api/races/statlines",
-            Statlines,
-        );
-    }
-
-    $: if (Statlines) {
-        sortedStatlines = [...Statlines].sort((a, b) => {
-            if (sortType === "all") {
-                if (a.name < b.name) {
-                    return 1;
-                }
-                if (a.name > b.name) {
-                    return -1;
-                }
-                return 0;
-            } else {
-                if (a.name < b.name) {
-                    return -1;
-                }
-                if (a.name > b.name) {
-                    return 1;
-                }
-                return 0;
-            }
-        });
-    }
-
-    function updateStatlines(updated) {
-        Statlines = Statlines.map((statline) => {
-            if (statline.isNew) {
-                return { ...updated, isNew: false };
-            }
-
-            if (statline.id === updated.id) {
-                return { ...statline, ...updated };
-            }
-
-            return statline;
-        });
-    }
-
-    function toggleSort() {
-        sortType = sortType === "all" ? "asc" : "all";
-    }
 </script>
 
-<div class="button-panel">
-    <button on:click={toggleSort}>
-        Sort {sortType === "all" ? "A -> Z" : "Z -> A"}
-    </button>
-    <button on:click={createStatline}> new statline </button>
-</div>
+<ListUtil
+    bind:list={Statlines}
+    endpoint={endpoint}
+    createRow={createStatline}
+    RowComponent={StatlineRow}
+/>
 
-<div>
-    {#each sortedStatlines as statline (statline.id)}
-        <StatlineRow
-            statline={statline}
-            onSave={updateStatlines}
-            onDelete={deleteStatline}
-        />
-    {/each}
-</div>
