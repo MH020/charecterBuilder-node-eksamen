b89fd4dfe481a7e50b38f2eb6bc6040a093e0164
Martin <166133291+MH020@users.noreply.github.com>
2025-12-23 21:33:06 +0100

    Update categoryList.svelte

--- frontend/src/componets/lists/ItemsList/categoryList.svelte
+++ frontend/src/componets/lists/ItemsList/categoryList.svelte
@@ -3,12 +3,13 @@
     import { fetchDelete, fetchGet } from "../../../../util/fetchUtil";
     import toastrDisplayHTTPCode from "../../../../util/ToastrUtil";
     import CategoryRow from "../../rows/CategoryRow.svelte";
+    import ListUtil from "../../util/ListUtil.svelte";
 
     let categoryies = [];
-    let sortType = "all";
+    let endpoint = "/api/Category"
 
     onMount(async () => {
-        const response = await fetchGet("/api/Category");
+        const response = await fetchGet(endpoint);
         if (response.status === 200) {
             categoryies = response.data;
         }
@@ -25,51 +26,11 @@
         categoryies = [...categoryies, newCategory];
     }
 
-    async function deleteCategory(category) {
-        if (!category.id) {
-            categoryies = categoryies.filter((c) => c !== category);
-            return;
-        }
-
-        const response = await fetchDelete("/api/category", category.id);
-        toastrDisplayHTTPCode(response.status, response.message);
-        if (response.status === 200) {
-            categoryies = categoryies.filter((c) => c.id !== category.id);
-        }
-    }
-
-    function updateCategory(updated) {
-        categoryies = categoryies.map((category) => {
-            if (category.isNew) {
-                return { ...updated, isNew: false };
-            }
-
-            if (category.id === updated.id) {
-                return { ...category, ...updated };
-            }
-
-            return category;
-        });
-    }
-
-    //sorting needed
-
-    function toggleSort() {}
 </script>
 
-<div class="button-panel">
-    <button on:click={toggleSort}>
-        sort by... {sortType === "all" ? "A -> Z" : "Z -> A"}
-    </button>
-    <button on:click={createCategory}> new category </button>
-</div>
-
-<div>
-    {#each categoryies as category (category.id)}
-        <CategoryRow
-            {category}
-            onDelete={deleteCategory}
-            onSave={updateCategory}
-        />
-    {/each}
-</div>
+<ListUtil
+    bind:list={categoryies}
+    endpoint={endpoint}
+    createRow={createCategory}
+    RowComponent={CategoryRow}
+/>
