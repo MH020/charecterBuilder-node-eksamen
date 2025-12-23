7dbd2d992bbc4eeb5691f13935bfc470d5dd38fe
Martin <166133291+MH020@users.noreply.github.com>
2025-12-23 21:33:16 +0100

    Update weaponsList.svelte

--- frontend/src/componets/lists/weaponLists/weaponsList.svelte
+++ frontend/src/componets/lists/weaponLists/weaponsList.svelte
@@ -3,9 +3,11 @@
     import WeaponsRow from '../../rows/weaponsRow.svelte';
     import { fetchGet } from '../../../../util/fetchUtil';
     import { deleteEntity} from '../../../../util/ListUtil';
+    import ListUtil from '../../util/ListUtil.svelte';
 
     let weapons = [];
     let sortType = "all"; 
+    const endpoint = "/api/weapons"
 
     onMount(async () => {
         const response = await fetchGet("/api/weapons")
@@ -37,54 +39,14 @@
         weapons = [...weapons, newWeapon];
         console.log(weapons)
     }
-
-    async function deleteWeapon(id, isNew = false) {
-        if(isNew){
-
-            weapons = weapons.filter(weaponTrait => weaponTrait.id !== id);
-            return;
-        }
-        weapons = await deleteEntity(id,"/api/weapon/traits",weapons);
-    }
-
-        function updateWeapons(updated) {
-        weapons = weapons.map((weapon) => {
-            if (weapon.isNew) {
-                return { ...updated, isNew: false };
-            }
-
-            if (weapon.id === updated.id) {
-                return { ...weapon, ...updated };
-            }
-
-            return weapon;
-        });
-    }
-
-    //sorting needed 
-
-    function toggleSort() {
-
-    }
 </script>
+<ListUtil
+    bind:list={weapons}
+    endpoint={endpoint}
+    createRow={createWeapon}
+    RowComponent={WeaponsRow}
+/>
 
-<div class="button-panel">
-    <button on:click={toggleSort}>
-        sort by... {sortType === "all" ? "A -> Z" : "Z -> A"}
-    </button>
-    <button on:click={createWeapon}>
-        new weapon 
-    </button>
-</div>
-
-<div>
-    {#each weapons as weapon (weapon.id)}
-        <WeaponsRow {weapon} 
-        onSave={updateWeapons}
-        onDelete={deleteWeapon}
-        />
-    {/each}
-</div>
 
 
 
