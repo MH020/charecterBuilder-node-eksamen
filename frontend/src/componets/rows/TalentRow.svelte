31647c6378b1c40bf2f7ba7909cfa6d3b08fc243
Martin <166133291+MH020@users.noreply.github.com>
2025-12-23 20:03:32 +0100

    Update TalentRow.svelte

--- frontend/src/componets/rows/TalentRow.svelte
+++ frontend/src/componets/rows/TalentRow.svelte
@@ -1,12 +1,16 @@
 <script>
     import { fetchPost, fetchUpdate } from "../../../util/fetchUtil";
+    import { selectMultibleFromModal } from "../../../util/ListUtil";
     import {
         closeModal,
         modalSelectCallback,
         openModal,
     } from "../../store/modalStore";
-    import StatlineCard from "../cards/StatlineCard.svelte";
-    import StatlinesList from "../lists/characterLists/StatlinesList.svelte";
+
+    import ApptitudesList from "../lists/ApptitudesList.svelte";
+    import LineageList from "../lists/characterLists/lineageList.svelte";
+    import RaceList from "../lists/characterLists/RaceList.svelte";
+    import TalentList from "../lists/characterLists/TalentList.svelte";
 
     export let talent;
     export let onSave;
@@ -16,6 +20,7 @@
     let isEditing = talent.isNew;
     let editableTalent = structuredClone(talent);
     let selectedSize = null;
+    let showTooltip;
 
     function startEditing() {
         isEditing = true;
@@ -50,60 +55,102 @@
         onDelete(editableTalent.id);
     }
 
-    function selectStatline(statline) {
-        modalSelectCallback.set((selectedStatline) => {
-            editableTalent = { ...editableTalent, [statline]: selectedStatline };
+    function selectElement(element, component) {
+        modalSelectCallback.set((selectedElement) => {
+            editableTalent = { ...editableTalent, [element]: selectedElement };
             modalSelectCallback.set(null);
         });
-        openModal(StatlinesList);
+        openModal(component);
     }
 
-    $: if (selectedSize) {
-        talent.size = selectedSize;
+    const types = [
+        "Half Talent",
+        "Full Talent",
+        "Class Feature Only",
+        "Expert Talent",
+    ];
+    let SelectedType = talent.type;
+
+    $: if (SelectedType) {
+        talent.type = SelectedType;
     }
 </script>
 
 <div class="row">
     {#if isEditing}
         <div class="row-fields">
-
             <div class="cell-box">
                 <div class="label">Name</div>
                 <input bind:value={editableTalent.name} />
-            </div>
-
-            <div class="select-box">
-                <div class="label">size</div>
-                <select bind:value={selectedSize}>
-                    <option value="" disabled selected>Select an size</option>
-                    {#each sizes as size}
-                        <option value={size}>{size.name}</option>
-                    {/each}
-                </select>
             </div>
         </div>
 
         <textarea bind:value={editableTalent.description} rows="3" cols="30"
         ></textarea>
 
-        <div class="cell-box">
-            <div class="label">Wounds</div>
-            <input type="number" bind:value={editableTalent.wounds} />
+        <div class="select-box">
+            <div class="label">type</div>
+            <select bind:value={editableTalent.type}>
+                <option value="" disabled selected>Select a type</option>
+                {#each types as type}
+                    <option value={type}>{type}</option>
+                {/each}
+            </select>
         </div>
 
-        <div class="statline-cards">
-            <div>
-                <button on:click={() => selectStatline("base_statline")}
-                    >Select Base Statline</button
-                >
-                <StatlineCard statline={editableTalent.base_statline} />
-            </div>
+        <div class="cell-box">
+            <div class="label">prerequisite talent</div>
+            <button
+                on:click={() =>
+                    selectElement("prerequisite_talent", TalentList)}
+            >
+                {editableTalent.prerequisite_talent.name ||
+                    "prerequisite talent"}
+            </button>
+        </div>
 
-            <div>
-                <button on:click={() => selectStatline("max_statline")}
-                    >Select Max Statline</button
-                >
-                <StatlineCard statline={editableTalent.max_statline} />
+        <div class="cell-box">
+            <div class="label">required race</div>
+            <button on:click={() => selectElement("required_race", RaceList)}>
+                {editableTalent.required_race.name || "required race"}
+            </button>
+        </div>
+
+        <div class="cell-box">
+            <div class="label">required lineage</div>
+            <button
+                on:click={() => selectElement("required_lineage", LineageList)}
+            >
+                {editableTalent.required_lineage.name || "required lineage"}
+            </button>
+        </div>
+
+        <button
+            on:click={() =>
+                selectMultibleFromModal(ApptitudesList, (selected) => {
+                    const list = editableTalent.aptitudes || [];
+
+                    const exists = list.some((c) => c.id === selected.id);
+
+                    editableTalent = {
+                        ...editableTalent,
+                        aptitudes: exists
+                            ? list.filter((a) => a.id !== selected.id)
+                            : [...list, selected],
+                    };
+                })}
+        >
+            select aptitudes
+        </button>
+
+        <div class="cell-box">
+            <div class="label">aptitudes</div>
+            <div class="tags">
+                {#each editableTalent.aptitudes as aptitude}
+                    <div>
+                        {aptitude.name || "----"}
+                    </div>
+                {/each}
             </div>
         </div>
 
@@ -135,78 +182,60 @@
                 <div class="label">Name</div>
                 <div>{talent.name || "----"}</div>
             </div>
+        </div>
 
+        <button
+            class="description-btn"
+            on:mouseenter={() => (showTooltip = true)}
+            on:mouseleave={() => (showTooltip = false)}
+            aria-describedby="tooltip-description"
+        >
+            description
+        </button>
+
+        {#if showTooltip}
+            <div id="tooltip-description" role="tooltip" class="tooltip">
+                {talent.description}
+            </div>
+        {/if}
+
+        <div class="row-fields">
             <div class="cell-box">
-                <div class="label">Size</div>
-                <div>{talent.size.name || "----"}</div>
+                <div class="label">type</div>
+                <div>{talent.type || "----"}</div>
+            </div>
+        </div>
+
+        <div class="row-fields">
+            <div class="cell-box">
+                <div class="label">prerequisite talent</div>
+                <div>{talent.prerequisite_talent.name || "none"}</div>
+            </div>
+        </div>
+
+        <div class="row-fields">
+            <div class="cell-box">
+                <div class="label">prerequisite talent</div>
+                <div>{talent.required_race.name || "-----"}</div>
+            </div>
+        </div>
+
+        <div class="row-fields">
+            <div class="cell-box">
+                <div class="label">required lineage</div>
+                <div>{talent.required_lineage.name || "-----"}</div>
             </div>
         </div>
 
         <div class="cell-box">
-            <div class="label">Description</div>
-            <div>{talent.description || "----"}</div>
-        </div>
-
-        <div class="cell-box">
-            <div class="label">Wounds</div>
-            <div>{talent.wounds || "----"}</div>
-        </div>
-
-        <div class="statline-cards">
-            <StatlineCard statline={talent.base_statline} />
-            <StatlineCard statline={talent.max_statline} />
+            <div class="label">aptitudes</div>
+            <div class="tags">
+                {#each editableTalent.aptitudes as aptitude}
+                    <div>
+                        {aptitude.name || "----"}
+                    </div>
+                {/each}
+            </div>
         </div>
     {/if}
 </div>
-
-<style>
-    .row {
-        display: flex;
-        flex-direction: column;
-        background-color: #1b1b1b;
-        border: 1px solid #444;
-        border-radius: 6px;
-        padding: 12px;
-        margin-bottom: 10px;
-        color: #e0e0e0;
-        font-family: "Courier New", monospace;
-        box-shadow: 0 2px 8px rgba(0, 0, 0, 0.7);
-    }
-
-    .row-fields {
-        display: flex;
-        gap: 1rem;
-    }
-
-    .cell-box {
-        display: flex;
-        flex-direction: column;
-        margin-bottom: 6px;
-    }
-
-    .label {
-        font-weight: bold;
-        color: #ffb86c;
-        margin-bottom: 2px;
-    }
-
-    input {
-        padding: 6px 8px;
-        border-radius: 4px;
-        border: 1px solid #333;
-        background-color: #121212;
-        color: #c0c0c0;
-    }
-
-    .buttons {
-        display: flex;
-        gap: 0.5rem;
-        margin-top: 8px;
-    }
-
-    .statline-cards {
-        display: flex;
-        gap: 1rem;
-        margin-top: 6px;
-    }
-</style>
