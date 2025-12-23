9c36e356badb0c5267ce3bc88414f03908daef3c
Martin <166133291+MH020@users.noreply.github.com>
2025-12-23 21:33:14 +0100

    Update weaponClassList.svelte

--- frontend/src/componets/lists/weaponLists/weaponClassList.svelte
+++ frontend/src/componets/lists/weaponLists/weaponClassList.svelte
@@ -3,14 +3,16 @@
     import { fetchGet } from '../../../../util/fetchUtil';
     import WeaponClassRow from '../../rows/weaponClassRow.svelte';
     import { deleteEntity } from '../../../../util/ListUtil';
+    import ListUtil from '../../util/ListUtil.svelte';
 
     let weaponClasses = [];
     let sortType = "all"; 
+    const endpoint = "/api/weapon/classes"
 
 
  
     onMount(async () => {
-        const response = await fetchGet("/api/weapon/classes")
+        const response = await fetchGet(endpoint)
         if (response.status === 200) {
                 weaponClasses = response.data
         } 
@@ -30,54 +32,15 @@
         weaponClasses = [...weaponClasses, newWeaponClass];
     }
 
-    async function deleteWeaponClass(id, isNew = false) {
-        if(isNew){
 
-            weaponClasses = weaponClasses.filter(weaponClass => weaponClass.id !== id);
-            return;
-        }
-        
-        weaponClasses = await deleteEntity(id,"/api/items",weaponClasses);
-    }
-
-        function updateWeaponClasses(updated) {
-        weaponClasses = weaponClasses.map((wc) => {
-            if (wc.isNew) {
-                return { ...updated, isNew: false };
-            }
-
-            if (wc.id === updated.id) {
-                return { ...wc, ...updated };
-            }
-
-            return wc;
-        });
-    }
-
-    //sorting needed 
-
-    function toggleSort() {
-
-    }
 </script>
 
-<div class="button-panel">
-    <button on:click={toggleSort}>
-        sort by... {sortType === "all" ? "A -> Z" : "Z -> A"}
-    </button>
-    <button on:click={createWeaponClass}>
-        new WeaponClass 
-    </button>
-</div>
-
-<div>
-    {#each weaponClasses as weaponClass (weaponClass.id)}
-        <WeaponClassRow {weaponClass} 
-        onSave={updateWeaponClasses}
-        onDelete={deleteWeaponClass}
-        />
-    {/each}
-</div>
+<ListUtil
+    bind:list={weaponClasses}
+    endpoint={endpoint}
+    createRow={createWeaponClass}
+    RowComponent={WeaponClassRow}
+/>
 
 
 
