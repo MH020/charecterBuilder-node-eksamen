653c741cf2ff6a0cff809d39720bfb1d9b924783
Martin <166133291+MH020@users.noreply.github.com>
2025-12-24 13:03:20 +0100

    Create DescriptionUtil.svelte

---
+++ frontend/src/componets/UI/DescriptionUtil.svelte
@@ -0,0 +1,19 @@
+        <script>
+            export let item
+            let showTooltip
+        </script>
+        
+        <button
+            class="description-btn"
+            on:mouseenter={() => (showTooltip = true)}
+            on:mouseleave={() => (showTooltip = false)}
+            aria-describedby="tooltip-description"
+        >
+            description
+        </button>
+
+        {#if showTooltip}
+            <div id="tooltip-description" role="tooltip" class="tooltip">
+                {item.description}
+            </div>
+        {/if}
