7b8b5cafc616ac221f01100dc564cd73bb49784d
Martin <166133291+MH020@users.noreply.github.com>
2025-12-24 13:03:13 +0100

    Update availabilityRow.svelte

--- frontend/src/componets/rows/availabilityRow.svelte
+++ frontend/src/componets/rows/availabilityRow.svelte
@@ -1,55 +1,33 @@
 <script>
-    import { fetchPost, fetchUpdate } from "../../../util/fetchUtil";
     import { closeModal, modalSelectCallback } from "../../store/modalStore";
+    import EditableRowSlot from "../wrappers/EditableRowSlot.svelte";
 
     export let availability;
     export let onSave;     
     export let onDelete;   
     export let endpoint
     
-    let isEditing = availability.isNew;
     let editableAvailability = structuredClone(availability);
-
-
-    function startEditing() {
-        isEditing = true;
-    }
-
-    async function saveEdit() {
-        let updated;
-
-        if (availability.isNew) {
-            const response = await fetchPost("/api/availability", editableAvailability);
-            if (response.status === 201) {
-                updated = response.created;
-            }
-        } else {
-            await fetchUpdate("/api/availability", editableAvailability);
-            updated = editableAvailability;
-        }
-
-        onSave(updated);   
-        isEditing = false;
-    }
-
-    function cancelEdit() {
-        isEditing = false;
-
-        if (availability.isNew) {
-
-            onDelete(availability.id, true); 
-        } else {
-            editableAvailability = structuredClone(availability);
-        }
-    }
-
-    function deleteRow() {
-        onDelete(availability.id);  
-    }
 
 </script>
 
 
+
+
+
+<EditableRowSlot
+
+  item={availability}
+  endpoint={endpoint}
+  onSave={onSave}
+  onDelete={onDelete}
+  bind:editable = {editableAvailability}
+  let:isEditing
+  let:startEditing
+  let:save
+  let:cancel
+  let:remove
+>
 <div class="row">
   {#if isEditing}
     <div class="cell-box">
@@ -58,8 +36,8 @@
     </div>
 
     <div class="buttons">
-      <button on:click={saveEdit}>Save</button>
-      <button on:click={cancelEdit}>Cancel</button>
+      <button on:click={save}>Save</button>
+      <button on:click={cancel}>Cancel</button>
     </div>
   {:else}
     <div class="cell-box">
@@ -70,10 +48,12 @@
     <div class="buttons">
         {#if !$modalSelectCallback}
             <button on:click={ startEditing}>Edit</button>
-            <button on:click={deleteRow}>Delete </button>
+            <button on:click={remove}>Delete </button>
         {:else}
             <button on:click={() => {$modalSelectCallback(availability); closeModal();}}> select</button>
         {/if}
     </div>
   {/if}
 </div>
+
+</EditableRowSlot>
