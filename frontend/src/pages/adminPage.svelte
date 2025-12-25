505004b1937f3c65b98b6d01cb4d6e1c732c3844
Martin <166133291+MH020@users.noreply.github.com>
2025-12-25 19:42:57 +0100

    Update adminPage.svelte

--- frontend/src/pages/adminPage.svelte
+++ frontend/src/pages/adminPage.svelte
@@ -1,7 +1,56 @@
 <script>
-    import Adminlayout from "../componets/layouts/adminlayout.svelte";
+// @ts-nocheck
+
+    import { Router, Route } from "svelte-routing";
+    import AdminNav from "../componets/nav/AdminNav.svelte";
+
+    import UsersList from "../componets/lists/usersList.svelte";
+    import SkillsList from "../componets/lists/skillsList.svelte";
+    import WeaponsList from "../componets/lists/weaponLists/weaponsList.svelte";
+    import CharectersheetsList from "../componets/lists/charectersheetsList.svelte";
+    import ApptitudesList from "../componets/lists/ApptitudesList.svelte";
+    import LineageList from "../componets/lists/characterLists/lineageList.svelte";
+    import RaceList from "../componets/lists/characterLists/RaceList.svelte";
+    import StatlinesList from "../componets/lists/characterLists/StatlinesList.svelte";
+    import SizeList from "../componets/lists/characterLists/sizeList.svelte";
+    import WeaponTraitList from "../componets/lists/weaponLists/weaponTraitList.svelte";
 </script>
 
-    <h2>Admin Page Content</h2>
-    
+<div class="admin-layout">
+    <AdminNav />
 
+    <Router basepath="/admin">
+        <section class="admin-content">
+
+
+            <Route path="users"><UsersList /></Route>
+            <Route path="charactersheets"><CharectersheetsList /></Route>
+            <Route path="skills"><SkillsList /></Route>
+            <Route path="aptitudes"><ApptitudesList /></Route>
+            <Route path="lineage"><LineageList /></Route>
+
+            <Route path="races"><RaceList /></Route>
+            <Route path="statlines"><StatlinesList /></Route>
+            <Route path="sizes"><SizeList /></Route>
+
+            <Route path="weapons"><WeaponsList /></Route>
+            <Route path="weapons/traits"><WeaponTraitList /></Route>
+            <Route path="weapons/class"><WeaponTraitList /></Route>
+
+            <Route path="items"><ItemsList /></Route>
+            <Route path="category"><CategoryList /></Route>
+            <Route path="availability"><AvailabilityList /></Route>
+            <Route path="craftsmanship"><CraftmanshipList /></Route>
+
+
+            <Route path="talents"><TalentList /></Route>
+            <Route path="traits"><TraitsList /></Route>
+
+
+
+            <Route path="armor"><ArmorList /></Route>
+
+            <Route path="class"><ClassList /></Route>
+        </section>
+    </Router>
+</div>
