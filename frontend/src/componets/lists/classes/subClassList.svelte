a60dd461c2690ddb077ddb9b386d2c54ccf3ef29
Martin <166133291+MH020@users.noreply.github.com>
2025-12-25 19:42:53 +0100

    Create subClassList.svelte

---
+++ frontend/src/componets/lists/classes/subClassList.svelte
@@ -0,0 +1,49 @@
+<script>
+    import { onMount } from 'svelte';
+    import { fetchGet } from '../../../../util/fetchUtil';
+    import { deleteEntity } from '../../../../util/ListUtil';
+    import ArmorRow from '../../rows/armorRow.svelte';
+    import ListUtil from '../../wrappers/EditableList.svelte';
+    import ClassRow from '../../rows/ClassRow.svelte';
+    
+    export let classID
+    let classes = [];
+    let endpoint = `/api/class/:${classID}/subclasses`
+
+    onMount(async () => {
+        const response = await fetchGet(endpoint);
+        console.log(response)
+        if (response.status === 200) {
+            classes = response.data;
+        }
+    });
+
+    function createClass() {
+        const newClass = {
+            id: null,  
+            name: "",  
+            description: "",
+            parent: {},
+            isNew: true
+        };
+        classes = [...classes, newClass];
+    }
+
+</script>
+
+<ListUtil
+    bind:list={classes}
+    endpoint= {endpoint}
+    createRow={createClass}
+    let:item
+    let:onSave
+    let:onDelete
+>
+    <ClassRow
+        clss={item}
+        onSave={onSave}
+        onDelete={onDelete}
+        endpoint={endpoint}
+    />
+</ListUtil>
+
