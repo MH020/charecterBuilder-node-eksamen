4d02ed492df4b029b9db2ee93df1358b899b903e
Martin <166133291+MH020@users.noreply.github.com>
2025-12-26 21:48:00 +0100

    Update ClassOverview.svelte

--- frontend/src/componets/subPages/ClassOverview.svelte
+++ frontend/src/componets/subPages/ClassOverview.svelte
@@ -1,43 +1,80 @@
 <script>
-
     import ClassesInfoList from "../UI/ClassesInfoList.svelte";
     import LevelTable from "../UI/levelTable.svelte";
 
     export let clss;
-    console.log(clss)
-    const classPowers = clss.powers || [];
-    const powersKnown = clss.powers_known || [];
-    const classTraits = clss.traits;
-    const classApptitudes = clss.aptitudes;
-    const classTalents = clss.talents;
-    const weaponsTrainings = clss.weapon_trainings;
-    const weaponsClasses = clss.weapon_classes;
-    const classBonuses = clss.bonuses || [];
-    const classItems = clss.items || [];
-    let subClassTalents = clss.SubclassTalents || []
+    console.log(clss);
+    $: classPowers = clss?.powers ?? [];
+    $: powersKnown = clss?.powers_known ?? [];
+    $: classTraits = clss?.traits ?? [];
+    $: classApptitudes = clss?.aptitudes ?? [];
+    $: classTalents = clss?.talents ?? [];
+    $: weaponsTrainings = clss?.weapon_trainings ?? [];
+    $: weaponsClasses = clss?.weapon_classes ?? [];
+    $: classBonuses = clss?.bonuses ?? [];
+    $: classItems = clss?.items ?? [];
+    $: subClassTalents = clss?.SubclassTalents ?? [];
 
-    let startingTraits = classTraits.filter((trait) => {
+    $: startingTraits = classTraits.filter((trait) => {
         trait.level = 1;
     });
 </script>
 
-<div class="class-name">
-    <h1>{clss.name}</h1>
-</div>
+{#if clss}
+    <div class="class-name">
+        <h1>{clss.name}</h1>
+    </div>
 
-<ClassesInfoList listItems={classBonuses} name="Characteristic Bonus" />
-<ClassesInfoList listItems={classApptitudes} name="Starting Aptitudes" />
-<ClassesInfoList listItems={weaponsClasses} name="Weapon Proficiency" />
-<ClassesInfoList listItems={weaponsTrainings} name="Weapon Training" />
-<ClassesInfoList listItems={classItems} name="Specialist Equipment" />
+    <div class="class-overview">
+        <div class="cell-box">
+            <div class="label">{"Characteristic Bonus:"}:</div>
+            <div class="tags">
+                {#each classBonuses as b}
+                    <h1>{b.characteristic.name + `${b.bonus}`}</h1>
+                {/each}
+            </div>
+        </div>
+    </div>
 
-{#if startingTraits.length !== 0}
-    <ClassesInfoList listItems={startingTraits} name="Starting Traits:" />
+        <div class="class-overview">
+        <div class="cell-box">
+            <div class="label">{"Starting Aptitudes:"}:</div>
+            <div class="tags">
+                {#each classApptitudes as a}
+                    <h1>{a.aptitude.name }</h1>
+                {/each}
+            </div>
+        </div>
+    </div>
+
+            <div class="class-overview">
+        <div class="cell-box">
+            <div class="label">{"Weapon Proficiency:"}:</div>
+            <div class="tags">
+                {#each weaponsClasses as wp}
+                    <h1>{wp.weapon_class.name }</h1>
+                {/each}
+            </div>
+        </div>
+    </div>
+
+    <div class="class-overview">
+        <div class="cell-box">
+            <div class="label">{"Weapon Training:"}:</div>
+            <div class="tags">
+                {#each weaponsTrainings as wt}
+                    <h1>{wt.talent.name }</h1>
+                {/each}
+            </div>
+        </div>
+    </div>
+
+
+    {#if startingTraits.length !== 0}
+
+    {/if}
+
+    <LevelTable {classTalents} {subClassTalents} {classPowers} />
+{:else}
+    <p>Loading class…</p>
 {/if}
-<ClassesInfoList listItems={weaponsTrainings} name="Weapon Training" />
-
-<LevelTable
-    classTalents={classTalents}
-    subClassTalents={subClassTalents}
-    {classPowers}
-/>
