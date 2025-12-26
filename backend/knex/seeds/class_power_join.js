2a934804d31a9859a07a971612451fb9b3c7cfd2
Martin <166133291+MH020@users.noreply.github.com>
2025-12-26 16:31:06 +0100

    Update class_power_join.js

--- backend/knex/seeds/class_power_join.js
+++ backend/knex/seeds/class_power_join.js
@@ -1,11 +1,8 @@
 /**
  * @param { import("knex").Knex } knex
- * @returns { Promise<void> }
  */
 export async function seed(knex) {
-
-  await knex('class_powers_known').del();
-
+  await knex('class_powers').del();
 
   const guardsman = await knex('class').where({ name: 'Guardsman' }).first();
   const tacticalMarine = await knex('class').where({ name: 'Tactical Marine' }).first();
@@ -13,32 +10,29 @@ export async function seed(knex) {
   const techPriest = await knex('class').where({ name: 'Tech-Priest Enginseer' }).first();
   const inquisitor = await knex('class').where({ name: 'Interrogator' }).first();
 
-
   const fireball = await knex('power').where({ name: 'Fireball' }).first();
   const iceSpike = await knex('power').where({ name: 'Ice Spike' }).first();
   const healingLight = await knex('power').where({ name: 'Healing Light' }).first();
   const shieldBash = await knex('power').where({ name: 'Shield Bash' }).first();
 
+  await knex('class_powers').insert([
+    // Guardsman
+    { class_id: guardsman.id, power_id: shieldBash.id, level: 1 },
 
-  await knex('class_powers_known').insert([
+    // Tactical Marine
+    { class_id: tacticalMarine.id, power_id: shieldBash.id, level: 1 },
+    { class_id: tacticalMarine.id, power_id: healingLight.id, level: 3 },
 
-    { class_id: guardsman.id, powers_known: 1, level: 1 },
-    { class_id: guardsman.id, powers_known: 1, level: 2 },
+    // Assault Marine
+    { class_id: assaultMarine.id, power_id: shieldBash.id, level: 1 },
+    { class_id: assaultMarine.id, power_id: fireball.id, level: 2 },
 
+    // Tech-Priest
+    { class_id: techPriest.id, power_id: healingLight.id, level: 1 },
+    { class_id: techPriest.id, power_id: iceSpike.id, level: 2 },
 
-    { class_id: tacticalMarine.id, powers_known: 1, level: 1 },
-    { class_id: tacticalMarine.id, powers_known: 2, level: 3 },
-
-
-    { class_id: assaultMarine.id, powers_known: 1, level: 1 },
-    { class_id: assaultMarine.id, powers_known: 2, level: 2 },
-
-
-    { class_id: techPriest.id, powers_known: 1, level: 1 },
-    { class_id: techPriest.id, powers_known: 1, level: 2 },
-
-  
-    { class_id: inquisitor.id, powers_known: 1, level: 1 },
-    { class_id: inquisitor.id, powers_known: 2, level: 2 }
+    // Inquisitor
+    { class_id: inquisitor.id, power_id: healingLight.id, level: 1 },
+    { class_id: inquisitor.id, power_id: fireball.id, level: 2 }
   ]);
 }
