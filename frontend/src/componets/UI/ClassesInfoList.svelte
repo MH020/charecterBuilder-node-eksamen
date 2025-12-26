05ece537d7af88fac864ad8842d04ae1b0d9b504
Martin <166133291+MH020@users.noreply.github.com>
2025-12-26 15:13:24 +0100

    Create ClassesInfoList.svelte

---
+++ frontend/src/componets/UI/ClassesInfoList.svelte
@@ -0,0 +1,15 @@
+<script>
+    export let name;
+    export let listItems;
+</script>
+
+<div class="class-overview">
+    <div class="cell-box">
+        <div class="label">{name}:</div>
+        <div class="tags">
+            {#each listItems as item}
+                <h1>{item.name}</h1>
+            {/each}
+        </div>
+    </div>
+</div>
