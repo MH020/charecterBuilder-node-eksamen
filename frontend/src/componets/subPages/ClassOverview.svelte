f6b3a5be4f857490f0cb4b7010726c22026ed896
Martin <166133291+MH020@users.noreply.github.com>
2025-12-26 15:13:23 +0100

    Update ClassOverview.svelte

--- frontend/src/componets/subPages/ClassOverview.svelte
+++ frontend/src/componets/subPages/ClassOverview.svelte
@@ -1,12 +1,43 @@
 <script>
-    export let clss
+    import { onMount } from "svelte";
+    import { fetchGet } from "../../../util/fetchUtil";
+    import ClassesInfoList from "../UI/ClassesInfoList.svelte";
+    import LevelTable from "../UI/levelTable.svelte";
+
+    export let clss;
+
+    let classApptitudes = [];
+    let classBonuses = [];
+    let classItems = [];
+    let classTraits = [];
+    let classTalents = []; 
+    let subClassTalents = []; 
+    let classPowers = []; 
+    let classes
+
+    onMount(async () => {
+        const response = await fetchGet(`/api/classes/${clss.id}/full`);
+        console.log(response)
+        if (response.status === 200) {
+            classes = response.data;
+        }
+    });
 </script>
 
 <div class="class-name">
-    <h1>{clss.name }</h1>
+    <h1>{clss.name}</h1>
 </div>
 
-<div class="cell-overview">
+<ClassesInfoList listItems={classApptitudes} name="Starting Aptitudes" />
+<ClassesInfoList listItems={classBonuses} name="Characteristic Bonus" />
+<ClassesInfoList listItems={classItems} name="Specialist Equipment" />
 
+{#if classTraits.length > 0}
+    <ClassesInfoList listItems={classTraits} name="Starting Traits" />
+{/if}
 
-</div>
+<LevelTable 
+classTalents = {classTalents},
+subClassTalents = {subClassTalents},
+classPowers = {}
+/>
