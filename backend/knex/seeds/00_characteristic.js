f90d8b3c07d88cab323f3c5bb9cc7f176c11b9b5
Martin <166133291+MH020@users.noreply.github.com>
2025-12-26 16:31:00 +0100

    Update 00_characteristic.js

--- backend/knex/seeds/00_characteristic.js
+++ backend/knex/seeds/00_characteristic.js
@@ -3,22 +3,39 @@
  * @returns { Promise<void> }
  */
 export async function seed (knex) {
-  // Deletes ALL existing entries
-  await knex('weapon_weapon_traits').del()
-  await knex('weapon_weapon_class').del()
-  await knex('talent_aptitude').del()
-  await knex('lineage_characteristic_bonus').del()
-  await knex('lineage_aptitude').del()
-  await knex('weapon_upgrade').del()
-  await knex('weapon').del()
-  await knex('item').del()
-  await knex('armor').del()
-  await knex('availability').del()
-  await knex('characteristic').del()
-  await knex('talent').del()
-  await knex('lineage').del()
-  await knex('trait').del()
-  await knex('race').del()
+// join tables 
+await knex('class_weapon_training').del()
+await knex('class_weapon_class').del()
+await knex('class_talents').del()
+await knex('class_aptitudes').del()
+await knex('class_bonuses').del()
+await knex('class_powers').del()
+await knex('class_powers_known').del()
+await knex('class_traits').del()
+await knex('class_items').del()
+
+await knex('talent_aptitude').del()
+await knex('prerequisite_powers').del()
+await knex('weapon_weapon_traits').del()
+await knex('weapon_weapon_class').del()
+await knex('lineage_characteristic_bonus').del()
+await knex('lineage_aptitude').del()
+
+// Core tables
+await knex('weapon_upgrade').del()
+await knex('weapon').del()
+await knex('item').del()
+await knex('armor').del()
+await knex('availability').del()
+await knex('characteristic').del()
+await knex('talent').del()
+await knex('lineage').del()
+await knex('trait').del()
+await knex('race').del()
+await knex('class').del()
+await knex('skill').del()
+await knex('aptitude').del()
+await knex('weapon_class').del()
 
   await knex('characteristic').insert([
     {
