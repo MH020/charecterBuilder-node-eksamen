3dbef92c3db170cf99c336de5d0d33e836551e86
Martin <166133291+MH020@users.noreply.github.com>
2025-12-26 15:13:03 +0100

    Update 08_weapons_class.js

--- backend/knex/seeds/08_weapons_class.js
+++ backend/knex/seeds/08_weapons_class.js
@@ -8,9 +8,10 @@ export async function seed (knex) {
 
 
   await knex('weapon_class').insert([
-    { name: 'Melee', description: 'Close combat weapons like swords, axes, and daggers' },
-    { name: 'Ranged', description: 'Weapons that attack from a distance like bows and firearms' },
-    { name: 'Explosive', description: 'Weapons that cause area damage like grenades and rockets' },
-    { name: 'Energy', description: 'Advanced energy-based weapons like laser rifles or plasma guns' }
+    { name: 'Pistol', description: 'Close combat weapons like swords, axes, and daggers' },
+    { name: 'Carbine', description: 'Weapons that attack from a distance like bows and firearms' },
+    { name: 'Long Rifle:', description: 'Weapons that cause area damage like grenades and rockets' },
+    { name: 'Heavy', description: 'Advanced energy-based weapons like laser rifles or plasma guns' },
+    { name: 'Super Heavy', description: 'Advanced energy-based weapons like laser rifles or plasma guns' }
   ])
 }
