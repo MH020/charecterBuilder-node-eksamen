124a8dbb9f2c0ad388b53c1f1b5e038d0f727587
Martin <166133291+MH020@users.noreply.github.com>
2025-12-26 15:13:14 +0100

    Create class_traits_join.js

---
+++ backend/knex/seeds/class_traits_join.js
@@ -0,0 +1,18 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+export async function seed(knex) {
+
+  await knex('class_traits').del();
+
+
+  const guardsman = await knex('class').where({ name: 'Guardsman' }).first();
+  const sheerDetermination = await knex('trait').where({ name: 'Sheer Determination' }).first();
+
+
+
+  await knex('class_traits').insert([
+    { class_id: guardsman.id, trait_id: sheerDetermination.id, level: 1 },
+  ]);
+}
