6627cd00e489e436cd3608f223d26e236363ddf6
Martin <166133291+MH020@users.noreply.github.com>
2025-12-26 16:31:18 +0100

    Update classRouter.js

--- backend/routers/classes/classRouter.js
+++ backend/routers/classes/classRouter.js
@@ -6,14 +6,14 @@ const router = Router()
 router.get('/api/classes', async (req, res) => {
     try {
         const result = await db.query(`
-        SELECT c.id, c.name, c.description,
-        json_build_object(
-            'id', p.id,
-            'name', p.name
-        ) AS parent
-        FROM "class" c
-        LEFT JOIN "class" c ON c.parent_id = p.id;
-        `)
+            SELECT c.id, c.name, c.description,
+            json_build_object(
+                'id', p.id,
+                'name', p.name
+            ) AS parent
+            FROM "class" c
+            LEFT JOIN "class" p ON c.parent_id = p.id;
+        `);
         const classes = result.rows
 
         return res.status(200).send(classes)
@@ -58,171 +58,155 @@ router.get('/api/classes/:id/full', async (req, res) => {
         `, [id])
 
 
-        const PowerResult = await db.query(`
-            SELECT json_agg(
+        const powerRows = await db.query(`
+            SELECT cp.id, cp.class_id, cp.level,
                 json_build_object(
-                    'id', cp.id,
-                    'class_id', cp.class_id,
-                    'level', cp.level,
-                    'power', json_build_object(
-                        'id', p.id,
-                        'name', p.name,
-                        'description', p.description,
-                        'ascendant', p.ascendant,
-                        'duration', p.duration,
-                        'actions', p.actions,
-                        'concentration', p.concentration,
-                        'dc', p.dc, 
-                        'range', p.range,
-                        'shape', p.shape
-                    ),
-                    'prerequisite_powers', (
-                        SELECT json_agg(
-                            json_build_object(
-                                'id', pp.id,
-                                'name', pp.name
-                            )
+                    'id', p.id,
+                    'name', p.name,
+                    'description', p.description,
+                    'ascendant', p.ascendant,
+                    'duration', p.duration,
+                    'actions', p.actions,
+                    'concentration', p.concentration,
+                    'dc', p.dc,
+                    'range', p.range,
+                    'shape', p.shape
+                ) AS power,
+                (
+                    SELECT json_agg(
+                        json_build_object(
+                            'id', pp.id,
+                            'name', pp.name
                         )
-                        FROM prerequisite_powers pr
-                        LEFT JOIN power pp ON pr.prerequisite_powers_id = pp.id
-                        WHERE pr.power_id = cp.power_id
                     )
-                )
-                ORDER BY cp.level
-            ) AS powers
+                    FROM prerequisite_powers pr
+                    LEFT JOIN power pp ON pr.prerequisite_powers_id = pp.id
+                    WHERE pr.power_id = cp.power_id
+                ) AS prerequisite_powers
+
             FROM class_powers cp
             LEFT JOIN power p ON cp.power_id = p.id
-            WHERE cp.class_id = $1;
-    `, [id]);
+            WHERE cp.class_id = $1
+            ORDER BY cp.level;
+        `, [id]);
 
 
-        const traitResult = await db.query(`
-        SELECT json_agg(
-            json_build_object(
-                'id', ct.id,
-                'class_id', ct.class_id,
-                'level', ct.level,
-                'trait', json_build_object(
+        const traitRows = await db.query(`
+            SELECT
+                ct.id,
+                ct.class_id,
+                ct.level,
+                json_build_object(
                     'id', t.id,
                     'name', t.name,
                     'description', t.description,
                     'has_input', t.has_input,
                     'category', t.category
-                )
-            )
-            ORDER BY ct.level
-        ) AS traits
-        FROM class_traits ct
-        LEFT JOIN trait t ON ct.trait_id = t.id
-        WHERE ct.class_id = $1;
+                ) AS trait
+            FROM class_traits ct
+            LEFT JOIN trait t ON ct.trait_id = t.id
+            WHERE ct.class_id = $1
+            ORDER BY ct.level;
         `, [id]);
 
 
-        const aptitudesResult = await db.query(`
-        SELECT json_agg(
-            json_build_object(
-                'id', ca.id,
-                'class_id', ca.class_id,
-                'aptitude', json_build_object(
-                    'id', t.id,
-                    'name', t.name
-                )
-            )
-            ORDER BY ca.id
-        ) AS aptitudes
-        FROM class_apptitudes ca
-        LEFT JOIN trait t ON ca.trait_id = t.id
-        WHERE ca.class_id = $1;
+
+        const aptitudesRows = await db.query(`
+            SELECT
+                ca.id,
+                ca.class_id,
+                json_build_object(
+                    'id', a.id,
+                    'name', a.name
+                ) AS aptitude
+            FROM class_aptitudes ca
+            LEFT JOIN aptitude a ON ca.aptitude_id = a.id
+            WHERE ca.class_id = $1
+            ORDER BY ca.id;
         `, [id]);
 
-        const talentsResult = await db.query(`
-        SELECT json_agg(
-            json_build_object(
-                'id', ct.id,
-                'class_id', ct.class_id,
-                'level', ct.level,
-                'talent', json_build_object(
+        const talentsRows = await db.query(`
+            SELECT
+                ct.id,
+                ct.class_id,
+                ct.level,
+                json_build_object(
                     'id', t.id,
                     'name', t.name,
                     'description', t.description,
                     'asi', t.asi
-                )
-            )
-            ORDER BY ct.level
-        ) AS talents
-        FROM class_talents ct
-        LEFT JOIN talent t ON ct.talent_id = t.id
-        WHERE ct.class_id = $1;
+                ) AS talent
+            FROM class_talents ct
+            LEFT JOIN talent t ON ct.talent_id = t.id
+            WHERE ct.class_id = $1
+            ORDER BY ct.level;
         `, [id]);
 
 
-        const weaponTrainingResult = await db.query(`
-        SELECT json_agg(
+    const weaponTrainingRows = await db.query(`
+        SELECT 
+            cwt.id,
+            cwt.class_id,
             json_build_object(
-                'id', cwt.id,
-                'class_id', cwt.class_id,
-                'talent', json_build_object(
-                    'id', t.id,
-                    'name', t.name,
-                    'description', t.description
-                )
-            )
-            ORDER BY cwt.id
-        ) AS weapon_trainings
+                'id', t.id,
+                'name', t.name,
+                'description', t.description
+            ) AS talent
         FROM class_weapon_training cwt
         LEFT JOIN talent t ON cwt.talent_id = t.id
-        WHERE cwt.class_id = $1;
-        `, [id]);
+        WHERE cwt.class_id = $1
+        ORDER BY cwt.id;
+    `, [id]);
 
 
-        const weaponClassResult = await db.query(`
-        SELECT json_agg(
-            json_build_object(
-                'id', cwc.id,
-                'class_id', cwc.class_id,
-                'weapon_class', json_build_object(
+        const weaponClassRows = await db.query(`
+            SELECT 
+                cwc.id, cwc.class_id,
+                json_build_object(
                     'id', wc.id,
                     'name', wc.name,
                     'description', wc.description
-                )
-            )
-            ORDER BY cwc.id
-        ) AS weapon_classes
-        FROM class_weapon_class cwc
-        LEFT JOIN weapon_class wc ON cwc.weapon_class = wc.id
-        WHERE cwc.class_id = $1;
+                ) AS weapon_class
+            FROM class_weapon_class cwc
+            LEFT JOIN weapon_class wc ON cwc.weapon_class_id = wc.id
+            WHERE cwc.class_id = $1
+            ORDER BY cwc.id;
         `, [id]);
 
 
-        const bonusesResult = await db.query(`
-        SELECT json_agg(
-            json_build_object(
-                'id', cb.id,
-                'class_id', cb.class_id,
-                'bonus', cb.bonus,
-                'characteristic', json_build_object(
+        const bonusesRows = await db.query(`
+            SELECT 
+                cb.id,
+                cb.class_id,
+                cb.bonus,
+                json_build_object(
                     'id', ch.id,
                     'name', ch.name,
                     'description', ch.description
-                )
-            )
-            ORDER BY cb.id
-        ) AS bonuses
-        FROM class_bonuses cb
-        LEFT JOIN characteristic ch ON cb.characteristic_id = ch.id
-        WHERE cb.class_id = $1;
+                ) AS characteristic
+            FROM class_bonuses cb
+            LEFT JOIN characteristic ch ON cb.characteristic_id = ch.id
+            WHERE cb.class_id = $1
+            ORDER BY cb.id;
+        `, [id]);
+
+
+        const powersKnownResult = await db.query(`
+        SELECT * FROM class_powers_known where (class_id = $1) 
+        ORDER BY class_powers_known.level
         `, [id]);
 
 
         const fullClass = {
             ...classResult.rows[0],
-            powers: PowerResult.rows[0],
-            traits: traitResult.rows[0],
-            aptitudes: aptitudesResult.rows[0],
-            talents: talentsResult.rows[0],
-            weapon_trainings: weaponTrainingResult.rows[0],
-            weapon_classes: weaponClassResult.rows[0],
-            bonuses: bonusesResult.rows[0],
+            powers: powerRows.rows,
+            powers_known: powersKnownResult.rows,
+            traits: traitRows.rows,
+            aptitudes: aptitudesRows.rows,
+            talents: talentsRows.rows,
+            weapon_trainings: weaponTrainingRows.rows,
+            weapon_classes: weaponClassRows.rows,
+            bonuses: bonusesRows.rows,
         };
 
         if (classResult.rows.length === 0) {
