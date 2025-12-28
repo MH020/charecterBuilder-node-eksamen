a524e84d9fa38d0f27569e6a1d33f777f06a9e8c
Martin <166133291+MH020@users.noreply.github.com>
2025-12-28 14:28:07 +0100

    Update userLayout.svelte

--- frontend/src/componets/layouts/userLayout.svelte
+++ frontend/src/componets/layouts/userLayout.svelte
@@ -13,21 +13,3 @@
     </div>
 </div>
 
-<style>
-.user-layout {
-    display: flex;
-    height: 100vh;
-}
-
-.user-main {
-    flex: 1;
-    display: flex;
-    flex-direction: column;
-}
-
-main {
-    flex: 1;
-    padding: 1rem;
-    overflow-y: auto;
-}
-</style>
