bdb94cca9a9100d09500ad1874454a5448870845
Martin <166133291+MH020@users.noreply.github.com>
2025-12-28 14:28:13 +0100

    Create classTalents.svelte

---
+++ frontend/src/componets/subPages/classTalents.svelte
@@ -0,0 +1,24 @@
+<script>
+    import TalentCard from "../cards/ClassTalentCard.svelte";
+
+    export let currentClass 
+
+    $: talents = currentClass.talents
+</script>
+
+    <div class="class-talents">
+            <div class="talents-cards">
+                {#each talents as thisTalent}
+                    <TalentCard Talent = {thisTalent} />
+                {/each}
+            </div>
+    </div>
+
+<style>
+.talents-cards {
+    display: grid;
+    grid-template-columns: repeat(3, 1fr); /* 3 cards per row */
+    gap: 5rem; /* space between cards */
+}
+</style>
+
