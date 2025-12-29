51ec7c9fbe34049b96f7b79321ead01f4efb1b46
Martin <166133291+MH020@users.noreply.github.com>
2025-12-29 12:55:56 +0100

    Create editableCardsPages.svelte

---
+++ frontend/src/componets/wrappers/editableCardsPages.svelte
@@ -0,0 +1,35 @@
+<script>
+    import { deleteEntity } from "../../../util/ListUtil";
+
+    export let list = [];
+    export let endpoint;
+    export let createCard;
+
+
+    async function onDelete(id, isNew) {
+        if (isNew) {
+            list = list.filter(item => item.id !== id);
+        } else {
+            list = await deleteEntity(id, endpoint, list);
+        }
+    }
+
+    function onSave(updated) {
+        list = list.map(item =>
+            item.isNew ? { ...updated, isNew: false }
+            : item.id === updated.id ? { ...item, ...updated }
+            : item
+        );
+    }
+</script>
+ <button on:click={createCard}>New Card</button>
+
+{#each list as card (card.id ?? card)}
+    <slot
+        item={card}
+        onSave={onSave}
+        onDelete={onDelete}
+    />
+{/each}
+
+
