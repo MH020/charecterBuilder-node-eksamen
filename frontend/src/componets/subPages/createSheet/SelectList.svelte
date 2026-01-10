8991931deeeb74ee5a26fd49c78936fd420f5910
Martin <166133291+MH020@users.noreply.github.com>
2026-01-10 14:01:48 +0100

    Create SelectList.svelte

---
+++ frontend/src/componets/subPages/createSheet/SelectList.svelte
@@ -0,0 +1,30 @@
+<script>
+    import { onMount } from "svelte";
+    import { fetchGet } from "../../../../util/fetchUtil";
+
+
+        export let endpoint
+        export let card
+        let items = null; 
+
+
+        onMount(async () => {
+        const response = await fetchGet(endpoint);
+        if (response.status === 200 ) {
+            items = response.data;
+            console.log(items)
+        }
+    });
+</script>
+
+
+{#if items !== null}
+{#each items as item}
+    <svelte:component this={card} item={item} />
+{/each}
+
+{:else}
+
+<h1>loading ....</h1>
+    
+{/if}
