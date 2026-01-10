52649f6fc0a70a6e0adcee52bb5deb7c53be2d4e
Martin <166133291+MH020@users.noreply.github.com>
2026-01-10 17:10:17 +0100

    Update createStore.js

--- frontend/src/store/createStore.js
+++ frontend/src/store/createStore.js
@@ -1,4 +1,8 @@
 import { writable } from "svelte/store";
 
+export const activeTabStore = writable("overview");
+export const buildingSheet = writable(false)
+
 export const selectedRace = writable(null);
 export const selectedLineage = writable(null);
+export const Selectedcharacteristics = writable(null);
