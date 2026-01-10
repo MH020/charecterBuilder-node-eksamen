0ac48d47538eb71b2ca6d871c9b4c9ba2633e11e
Martin <166133291+MH020@users.noreply.github.com>
2026-01-10 15:27:14 +0100

    Update userStore.js

--- frontend/src/store/userStore.js
+++ frontend/src/store/userStore.js
@@ -1,4 +1,26 @@
+import { navigate } from 'svelte-routing';
 import { writable } from 'svelte/store'
+import { fetchGet } from '../../util/fetchUtil';
 
-export const user = writable({ username: '', email: '', role: '' })
+export const user = writable({ id: null, username: '', email: '', role: '' })
 export const loading = writable(true)
+
+
+export async function refresh() {
+  try {
+    const res = await fetchGet("/api/me")
+
+    console.log(res)
+
+    if (res.status === 401) {
+      user.set({ id: null, username: '', email: '', role: '' });
+      navigate('/login');
+      return;
+    }
+    user.set(res.data.user);
+  } catch (err) {
+    console.log(err)
+    user.set({ id: null, username: '', email: '', role: '' });
+    navigate('/login');
+  } 
+}
