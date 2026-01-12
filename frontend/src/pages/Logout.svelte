d47e0133edf17b1bbd77d05acaad001f5553213f
Martin <166133291+MH020@users.noreply.github.com>
2026-01-12 20:25:42 +0100

    Create Logout.svelte

---
+++ frontend/src/pages/Logout.svelte
@@ -0,0 +1,14 @@
+<script>
+    import { onMount, tick } from "svelte";
+    import { fetchPost } from "../../util/fetchUtil";
+    import { navigate } from "svelte-routing";
+    import { user } from "../store/userStore";
+
+    onMount(async () => {
+
+    await fetchPost("/api/logout")
+    user.set({ id: null, username: '', email: '', role: '' });
+    await tick();
+    navigate("/")
+    });
+</script>
