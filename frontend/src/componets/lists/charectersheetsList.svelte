b5aaa15fbdf254d7fd0c84ffc4a347a4d4eab440
Martin <166133291+MH020@users.noreply.github.com>
2026-01-12 18:43:57 +0100

    Update charectersheetsList.svelte

--- frontend/src/componets/lists/charectersheetsList.svelte
+++ frontend/src/componets/lists/charectersheetsList.svelte
@@ -1,2 +1,42 @@
 <script>
+    import { onMount } from "svelte";
+    import { fetchDelete, fetchGet } from "../../../util/fetchUtil";
+    import SheetCard from "../cards/SheetCard.svelte"
+    import toastrDisplayHTTPCode from "../../../util/ToastrUtil";
+    import { refresh } from "../../store/userStore";
+
+
+
+    let sheets = [];
+    let endpoint = "/api/charectersheets"
+
+    onMount(async () => {
+        refresh()
+        const response = await fetchGet(endpoint);
+        console.log(response)
+        if (response.status === 200) {
+            sheets = response.data;
+        }
+    });
+
+
+    async function onDelete(id) {
+        const response = await fetchDelete(endpoint, id);
+
+        toastrDisplayHTTPCode(response.status, response.message);
+
+        if (response.status === 200) {
+            sheets = sheets.filter(sheet => sheet.id !== id);
+        }
+    }
+    
+
 </script>
+
+{#each sheets as sheet (sheet.id)}
+    <SheetCard
+    sheet = {sheet} 
+    onDelete = {onDelete}
+    />
+{/each}
+
