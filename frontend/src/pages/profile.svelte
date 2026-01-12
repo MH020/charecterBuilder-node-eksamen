2023cac9aacac52e5c495ec676add005a45a4fd7
Martin <166133291+MH020@users.noreply.github.com>
2026-01-12 20:03:04 +0100

    Update profile.svelte

--- frontend/src/pages/profile.svelte
+++ frontend/src/pages/profile.svelte
@@ -1,30 +1,18 @@
 <script>
   import { onMount } from 'svelte';
-  import { navigate } from 'svelte-routing';
-  import { fetchGet } from '../../util/fetchUtil';
-  import { user, loading } from '../store/userStore';
-  import toastrDisplayHTTPCode from "../../util/ToastrUtil.js"
+  import { user, refresh } from '../store/userStore';
+
 
   onMount(async () => {
-    const response = await fetchGet('/users/id')
-    console.log(response)
-    if (response.status !== 200) {
-      toastrDisplayHTTPCode(response.status,response.message);
-      navigate('/login');
-    } else {
-      user.set(response.data)
-      loading.set(false);
-    }
+    refresh()
   });
+
 </script>
 
-{#if $loading}
-  <p>Loading profile</p>
-{:else}
+
   <div class="profile">
     <h1>Profile</h1>
     <p><strong>Username:</strong> {$user.username}</p>
     <p><strong>Email:</strong> {$user.email}</p>
     <p><strong>Role:</strong> {$user.role}</p>
   </div>
-{/if}
