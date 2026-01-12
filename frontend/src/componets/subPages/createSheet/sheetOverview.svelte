e0727ddcb79998d792cfa225aece252d48d11dc1
Martin <166133291+MH020@users.noreply.github.com>
2026-01-12 16:35:02 +0100

    Create sheetOverview.svelte

---
+++ frontend/src/componets/subPages/createSheet/sheetOverview.svelte
@@ -0,0 +1,41 @@
+<script>
+
+export let sheet
+
+    $: characteristics = sheet?.characteristics ?? [];
+
+
+    function bonus(value) {
+        return Math.floor((value - 10) / 2);
+    }
+</script>
+
+
+    <div class="class-name">
+        <h1>{sheet.name}</h1>
+    </div>
+
+
+    <div class="sheet-container">
+
+<div class="sheet-CHARACTERISTICS">
+    <h1>CHARACTERISTICS</h1>
+
+    <div class="characteristics-grid">
+        {#each characteristics as stat}
+            <div class="characteristic-box">
+                <div class="stat-name">{stat.name}</div>
+
+                <div class="stat-value">{stat.value}</div>
+
+                <div class="stat-bonus">
+                    {bonus(stat.value) >= 0
+                        ? `+${bonus(stat.value)}`
+                        : bonus(stat.value)}
+                </div>
+            </div>
+        {/each}
+    </div>
+</div>
+
+    </div>
