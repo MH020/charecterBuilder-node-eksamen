67f0be5526bda460ec9d56a1e4157b2a2704af06
Martin <166133291+MH020@users.noreply.github.com>
2026-01-12 20:02:59 +0100

    Update AdminNav.svelte

--- frontend/src/componets/nav/AdminNav.svelte
+++ frontend/src/componets/nav/AdminNav.svelte
@@ -6,7 +6,5 @@
   <Link to="/">home</Link>
   <Link to="/logout">logout</Link>
   <Link to="/profile">Profile</Link>
-  <Link to="/players">Players</Link>
-  <Link to="/campaigns">Campaigns</Link>
-  <Link to="/admin page">Admin Page</Link>
+  <Link to="/admin">Admin Page</Link>
 </nav>
